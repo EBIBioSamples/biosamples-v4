6e1ba2f2e7e9385e501d74002cd59ed3b3f54da7
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-05-25 15:44:40 +0100

    Removed unnused code

--- subs/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ subs/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -1,14 +1,12 @@
 package uk.ac.ebi.biosamples;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
+
 
 @SpringBootApplication
 public class Application {
@@ -17,17 +15,7 @@ public class Application {
 	public Queue queue() {
 		return new Queue(Messaging.queueToBeLoaded, true);
 	}
-/*
-	@Bean
-	public TopicExchange exchange() {
-		return new TopicExchange(Messaging.exchangeBioSamples);
-	}
-
-	@Bean
-	public Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with(Messaging.queueToBeLoaded);
-	}
-*/
+	
 	@Bean
 	public MessageConverter messageConverter() {
 		return new MappingJackson2MessageConverter();
