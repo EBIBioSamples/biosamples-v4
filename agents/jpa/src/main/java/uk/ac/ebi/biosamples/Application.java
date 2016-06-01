c0bdf1b831f2924c3be6948abaf050ccaca6531a
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-06-01 09:28:20 +0100

    Cleanup, propegate exit codes

--- agents/jpa/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ agents/jpa/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -1,16 +1,13 @@
 package uk.ac.ebi.biosamples;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.converter.MessageConverter;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		System.exit(SpringApplication.exit(SpringApplication.run(Application.class, args)));
+		//SpringApplication.run(Application.class, args);
 	}
 }
