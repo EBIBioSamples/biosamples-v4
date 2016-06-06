55c7e17e678b9bef01eb313ca9e3d9e08aa95915
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-06-06 14:00:48 +0100

    Added sample accession uniqueness constraint

--- models/jpa/src/main/java/uk/ac/ebi/biosamples/models/JPASample.java
+++ models/jpa/src/main/java/uk/ac/ebi/biosamples/models/JPASample.java
@@ -12,7 +12,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "ACCESSION" }) ) 
 @Entity
 public class JPASample implements Sample {
 
