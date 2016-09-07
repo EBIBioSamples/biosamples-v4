9700aa78399d7c0ec4d0706acc26c2bbabc9c6ec
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-09-07 15:06:50 +0100

    Use paging correctly

--- agents/jpa/src/main/java/uk/ac/ebi/biosamples/MessageHandlerJPA.java
+++ agents/jpa/src/main/java/uk/ac/ebi/biosamples/MessageHandlerJPA.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -58,6 +60,7 @@ public class MessageHandlerJPA {
 
 			// TODO check old sample if present
 
+			Pageable pageable = new PageRequest(0,10);
 			Iterable<JPAAttribute> oldAttributes = jpaAttributeRepository.findByKeyAndValueAndUnitAndOntologyTerm(type,
 					value, unit, ontologyTerm);
 
