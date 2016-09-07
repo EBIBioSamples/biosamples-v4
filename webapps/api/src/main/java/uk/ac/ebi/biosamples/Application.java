0c9b2fa3ad323e6fd5eb98088c4beaeb091f6177
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-09-07 15:06:25 +0100

    Moved rest documentation to rest-doc branch

--- webapps/api/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ webapps/api/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -5,12 +5,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.core.config.EnumTranslationConfiguration;
-import org.springframework.data.rest.core.config.MetadataConfiguration;
-import org.springframework.data.rest.core.config.ProjectionDefinitionConfiguration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 
