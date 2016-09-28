4b10aa84173b578d51f353bde71314e55520b2bd
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-09-28 14:42:29 +0100

    remove ncbi ftp access

--- pipelines/src/main/java/uk/ac/ebi/biosamples/PipelinesProperties.java
+++ pipelines/src/main/java/uk/ac/ebi/biosamples/PipelinesProperties.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.biosamples;
 
-import java.io.File;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -8,15 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PipelinesProperties {
-	
-	@Value("${biosamples.ncbi.server:ftp.ncbi.nlm.nih.gov}")
-	private String ncbiFTPserver;
-	
-	@Value("${biosamples.ncbi.localfile:ncbi.xml.gz}")
-	private File ncbiLocalFile;
-	
-	@Value("${biosamples.ncbi.remotepath:/biosample/biosample_set.xml.gz}")
-	private String ncbiRemotePath;
 	
 	@Value("${biosamples.ncbi.httpuri:http://ftp.ncbi.nlm.nih.gov/biosample/biosample_set.xml.gz}")
 	private URI ncbiHttpUri;
@@ -26,18 +16,6 @@ public class PipelinesProperties {
 	
 	@Value("${biosamples.submissionuri:http://localhost:8083/}")
 	private URI biosampleSubmissionURI;
-	
-	public String getNCBIFTPServer() {
-		return ncbiFTPserver;
-	}
-	
-	public File getNCBILocalFile() {
-		return ncbiLocalFile.getAbsoluteFile();
-	}
-	
-	public String getNCBIRemotePath() {
-		return ncbiRemotePath;
-	}
 
 	public URI getBiosampleSubmissionURI() {
 		return biosampleSubmissionURI;
