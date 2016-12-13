2493cbdc56a2cddf72eb94e1dc837ef6cdde7318
Adam Faulconbridge <afaulconbridge@googlemail.com>
2016-12-13 10:36:48 +0000

    added id converter and cache control headers

--- webapps/api/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ webapps/api/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -1,15 +1,9 @@
 package uk.ac.ebi.biosamples;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.MappedInterceptor;
 
 @SpringBootApplication
@@ -21,27 +15,7 @@ public class Application extends SpringBootServletInitializer {
 	
 	@Bean
 	public MappedInterceptor getCacheHeaderMappedInterceptor() {
-	    return new MappedInterceptor(new String[]{"/**"}, new HandlerInterceptor(){
-	    	
-			@Override
-			public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-					throws Exception {
-				return true;
-			}
-
-			@Override
-			public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-					ModelAndView modelAndView) throws Exception {
-				//allow response to be cached for one hour
-				response.addHeader(HttpHeaders.CACHE_CONTROL, "max-age="+Integer.toString(60*60));
-			}
-
-			@Override
-			public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-					Exception ex) throws Exception {
-				// do nothing
-				
-			}});
+	    return new MappedInterceptor(new String[]{"/**"}, new CacheControlInterceptor());
 	}
 	
 }
---
+++ webapps/api/src/main/java/uk/ac/ebi/biosamples/CacheControlInterceptor.java
@@ -0,0 +1,45 @@
+package uk.ac.ebi.biosamples;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Interceptor to add Cache-Control headers to GET requests
+ * 
+ * Not useful for spring data rest repositories that use @LastModified annotations
+ * 
+ * @author faulcon
+ *
+ */
+public class CacheControlInterceptor implements HandlerInterceptor {
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		//only cache get requests
+		if (request.getMethod().equals("GET")) {
+			//allow response to be cached for one hour
+			response.addHeader(HttpHeaders.CACHE_CONTROL, "max-age=3600");
+		}
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex) throws Exception {
+		// do nothing
+		
+	}
+}
