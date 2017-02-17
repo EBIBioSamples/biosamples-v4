0b82a1e90c375c5ad59b62dd08ea8945bc7b6311
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-02-17 11:46:12 +0000

    added agent stayalive properties

--- agents/neo4j/src/main/java/uk/ac/ebi/biosamples/Neo4JRunner.java
+++ agents/neo4j/src/main/java/uk/ac/ebi/biosamples/Neo4JRunner.java
@@ -8,22 +8,25 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LiveRunner implements ApplicationRunner {
+public class Neo4JRunner implements ApplicationRunner {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private MessageUtils messageUtils;
+	
+	@Autowired
+	private AgentNeo4JProperties agentNeo4JProperties;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// as long as there are messages to read, keep this thread alive
 		// that will also keep the async message client alive too?
 		Integer messageCount = null;
-		while (messageCount == null || messageCount > 0) {
+		while (agentNeo4JProperties.getAgentNeo4JStayalive() || messageCount == null || messageCount > 0) {
 			Thread.sleep(1000*60);
 			messageCount = messageUtils.getQueueCount(Messaging.queueToBeIndexedNeo4J);
-			log.trace("Messages remaining in "+Messaging.queueToBeIndexedNeo4J+" "+messageCount);
+			log.info("Messages remaining in "+Messaging.queueToBeIndexedNeo4J+" "+messageCount);
 		}
 		
 	}
--- agents/solr/src/main/java/uk/ac/ebi/biosamples/SolrRunner.java
+++ agents/solr/src/main/java/uk/ac/ebi/biosamples/SolrRunner.java
@@ -2,25 +2,30 @@ package uk.ac.ebi.biosamples;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LiveRunner implements ApplicationRunner {
+public class SolrRunner implements ApplicationRunner {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private MessageUtils messageUtils;
+	
+	@Autowired
+	private AgentSolrProperties agentSolrProperties;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// as long as there are messages to read, keep this thread alive
 		// that will also keep the async message client alive too?
 		Integer messageCount = null;
-		while (messageCount == null || messageCount > 0) {
+		while (agentSolrProperties.getAgentSolrStayalive() || messageCount == null || messageCount > 0) {
 			Thread.sleep(1000*60);
 			messageCount = messageUtils.getQueueCount(Messaging.queueToBeIndexedSolr);
 			log.info("Messages remaining in "+Messaging.queueToBeIndexedSolr+" "+messageCount);
