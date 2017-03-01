881c6a758880d966ef16944bb97ba4adc6421bff
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-03-01 16:24:06 +0000

    removed explicit multicore solr reference

--- agents/solr/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ agents/solr/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -1,13 +1,15 @@
 package uk.ac.ebi.biosamples;
 
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.solr.core.SolrOperations;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableSolrRepositories(multicoreSupport = true)
 @EnableAsync
 @EnableScheduling
 public class Application {	
