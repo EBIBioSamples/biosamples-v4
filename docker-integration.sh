e1747e35308c98548607770ee4237fa357c83b9a
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-03-01 14:05:39 +0000

    call curation agent

--- docker-integration.sh
+++ docker-integration.sh
@@ -15,10 +15,10 @@ docker-compose up -d biosamples-webapps-core biosamples-webapps-sampletab
 
 java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase1
 
-docker-compose up -d biosamples-agents-neo4j biosamples-agents-solr
+docker-compose up -d biosamples-agents-neo4j biosamples-agents-solr biosamples-agents-curation
 
-echo "sleeping for 30 seconds..."
-sleep 30
+echo "sleeping for 10 seconds..."
+sleep 10
 
 java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase2
 
