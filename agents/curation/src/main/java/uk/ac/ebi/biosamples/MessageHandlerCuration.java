9dddd5d4ee7e1c0825fd0747769c4ff40a50ad6c
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-03-01 09:24:05 +0000

    link curation to client

--- agents/curation/src/main/java/uk/ac/ebi/biosamples/MessageHandlerCuration.java
+++ agents/curation/src/main/java/uk/ac/ebi/biosamples/MessageHandlerCuration.java
@@ -1,15 +1,38 @@
 package uk.ac.ebi.biosamples;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.biosamples.model.Sample;
+import uk.ac.ebi.biosamples.service.SubmissionService;
 
 @Service
 public class MessageHandlerCuration {
+	
+	@Autowired
+	public SubmissionService submissionService;
 
 	@RabbitListener(queues = Messaging.queueToBeCurated)
-	public void handle(Sample sample) {
+	public void handle(Sample sample) {		
+		
+		boolean changed = false;
+
 		//TODO write me
+				
+		//query un-mapped attributes against Zooma
+		
+		//validate existing ontology terms against OLS
+		
+		//turn attributes with biosample accessions into relationships
+		
+		//split number+unit attributes
+		
+		//lowercase attribute types		
+		//lowercase relationship types
+		
+		if (changed) {
+			//if we did any changes, send the updated version to the interface
+		}
 	}
 
 }
