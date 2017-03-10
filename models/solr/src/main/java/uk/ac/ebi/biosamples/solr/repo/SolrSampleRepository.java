0230da7168dac1906bc5fe059a5fd3f20b2b15fc
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-03-10 11:28:17 +0000

    remove unused methods

--- models/solr/src/main/java/uk/ac/ebi/biosamples/solr/repo/SolrSampleRepository.java
+++ models/solr/src/main/java/uk/ac/ebi/biosamples/solr/repo/SolrSampleRepository.java
@@ -14,19 +14,11 @@ public interface SolrSampleRepository extends SolrCrudRepository<SolrSample, Str
 	@Query(value = "?0")
 	FacetPage<SolrSample> findByText(String text, Pageable page);
 	@Query(value = "?0")
-	@Facet(fields = { "organism_av_ss" }, limit = 10)
-	FacetPage<SolrSample> findByTextWithFacets(String text, Pageable page);
-	@Query(value = "?0")
 	@Facet(fields = { "attributetypes_ss" }, limit = 10)
 	FacetPage<SolrSample> findByTextWithFacetTypes(String text, Pageable page);
 
 	@Query(value = "?0 AND release_dt:[* TO NOW]")
 	FacetPage<SolrSample> findByTextAndPublic(String text, Pageable page);
-	@Query(value = "?0 AND release_dt:[* TO NOW]")
-	@Facet(fields = { "?1", "?2", "?3", "?4", "?5", "?6", "?7", "?8", "?9", "?10"}, limit = 10)
-	FacetPage<SolrSample> findByTextAndPublicWithFacets(String text, String facet1, String facet2, 
-			String facet3, String facet4, String facet5, String facet6, String facet7, String facet8, 
-			String facet9, String facet10, Pageable page);
 	@Query(value = "?0 AND release_dt:[* TO NOW]")
 	@Facet(fields = { "attributetypes_ss" }, limit = 10)
 	FacetPage<SolrSample> findByTextAndPublicWithFacetTypes(String text, Pageable page);
