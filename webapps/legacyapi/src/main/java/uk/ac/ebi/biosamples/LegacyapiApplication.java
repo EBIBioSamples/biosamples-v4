6ca64283d6dca26ffd0b0cf7f6d653f168fdd0a8
Kerruba <luca.cherubin@gmail.com>
2017-03-29 16:21:27 +0100

    Started legacy api ticket

---
+++ webapps/legacyapi/src/main/java/uk/ac/ebi/biosamples/LegacyapiApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.biosamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyapiApplication.class, args);
+	}
+}
