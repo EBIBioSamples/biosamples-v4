5d84e19d1e36ccc7d18a9ca05e20eeb8c64e2a16
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-04-12 10:49:30 +0100

    moved accessioning into mongo

---
+++ models/mongo/src/main/java/uk/ac/ebi/biosamples/mongo/service/MongoAccessionService.java
@@ -0,0 +1,88 @@
+package uk.ac.ebi.biosamples.mongo.service;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.ErrorCategory;
+import com.mongodb.MongoWriteException;
+
+import uk.ac.ebi.biosamples.mongo.MongoProperties;
+import uk.ac.ebi.biosamples.mongo.model.MongoSample;
+import uk.ac.ebi.biosamples.mongo.repo.MongoSampleRepository;
+
+@Service
+public class MongoAccessionService {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	@Autowired
+	private MongoSampleRepository mongoSampleRepository;
+
+	private BlockingQueue<String> accessionCandidateQueue;;
+	private long accessionCandidateCounter;
+	
+	@Autowired
+	private MongoProperties mongoProperties;
+
+	@PostConstruct
+	public void doSetup() {
+		accessionCandidateQueue = new LinkedBlockingQueue<>(mongoProperties.getAcessionQueueSize());
+		accessionCandidateCounter = mongoProperties.getAccessionMinimum();
+	}
+
+	public MongoSample accessionAndInsert(MongoSample sample) {
+		// inspired by Optimistic Loops of
+		// https://docs.mongodb.com/v3.0/tutorial/create-an-auto-incrementing-field/
+		boolean success = false;
+		// TODO limit number of tries
+		while (!success) {
+			// TODO add a timeout here
+			try {
+				sample = MongoSample.build(sample.getName(), accessionCandidateQueue.take(), 
+						sample.getRelease(), sample.getUpdate(), sample.getAttributes(), sample.getRelationships(), sample.getExternalReferences());
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+
+			try {
+				sample = mongoSampleRepository.insertNew(sample);
+				success = true;
+			} catch (MongoWriteException e) {
+				if (e.getError().getCategory() == ErrorCategory.DUPLICATE_KEY) {
+					success = false;
+					sample = MongoSample.build(sample.getName(), null, 
+							sample.getRelease(), sample.getUpdate(), sample.getAttributes(), sample.getRelationships(), sample.getExternalReferences());
+				} else {
+					throw e;
+				}
+			}
+		}
+		return sample;
+	}
+
+	@Scheduled(fixedDelay = 100)
+	public void prepareAccessions() {
+		while (accessionCandidateQueue.remainingCapacity() > 0) {
+			String accessionCandidate = mongoProperties.getAccessionPrefix() + accessionCandidateCounter;
+			// if the accession already exists, skip it
+			if (mongoSampleRepository.exists(accessionCandidate)) {
+				accessionCandidateCounter += 1;
+				// if the accession can't be put in the queue at this time
+				// (queue full), stop
+			} else if (!accessionCandidateQueue.offer(accessionCandidate)) {
+				return;
+			} else {
+				//put it into the queue, move on to next
+				accessionCandidateCounter += 1;
+			}
+		}
+	}
+}
