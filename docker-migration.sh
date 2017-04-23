374d1353cf8aeec5b6f5af9acc09f9e020df9312
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-04-23 11:25:58 +0100

    change content type

--- docker-migration.sh
+++ docker-migration.sh
@@ -17,17 +17,17 @@ docker-compose scale biosamples-agents-solr=5
 echo "Importing SampleTab submissions"
 export SUBS_HOME=/tmp/submissions
 #rsync -zarv --prune-empty-dirs --include="*/" --include="sampletab.pre.txt" --exclude="*" ebi-cli.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/GSB/ $SUBS_HOME
-ls $SUBS_HOME/*/sampletab.pre.txt | xargs -n 1 -P 8 -I {} curl -X POST -H "Content-Type: text/plain" --data-binary @{} http://localhost:8082/biosamples/beta/sampletab/v4
+ls $SUBS_HOME/*/sampletab.pre.txt | xargs -n 1 -P 8 -I {} curl -X POST -H "Content-Type: application/text" --data-binary @{} http://localhost:8082/biosamples/beta/sampletab/v4
 echo "Imported SampleTab submissions"
 
 #import from ena
-echo "Importing from ENA"
-time java -jar pipelines/ena/target/pipelines-ena-4.0.0-SNAPSHOT.jar --biosamples.submissionuri=http://localhost:8081/biosamples/beta  --logging.file=./docker/logs/pipelines-ena.log
-echo "Imported from ENA"
+#echo "Importing from ENA"
+#time java -jar pipelines/ena/target/pipelines-ena-4.0.0-SNAPSHOT.jar --biosamples.client.uri=http://localhost:8081/biosamples/beta  --logging.file=./docker/logs/pipelines-ena.log
+#echo "Imported from ENA"
 
 #import from NCBI
 echo "Importing from NCBI"
-wget -O /tmp/biosample_set.xml.gz http://ftp.ncbi.nih.gov/biosample/biosample_set.xml.gz 
-time java -jar pipelines/ncbi/target/pipelines-ncbi-4.0.0-SNAPSHOT.jar --biosamples.submissionuri=http://localhost:8081/biosamples/beta --biosamples.ncbi.file=/tmp/biosample_set.xml.gz --logging.file=./docker/logs/pipelines-ncbi.log
+#wget -O /tmp/biosample_set.xml.gz http://ftp.ncbi.nih.gov/biosample/biosample_set.xml.gz 
+time java -jar pipelines/ncbi/target/pipelines-ncbi-4.0.0-SNAPSHOT.jar --biosamples.client.uri=http://localhost:8081/biosamples/beta --biosamples.ncbi.file=/home/faulcon/Desktop/biosample_set.xml.gz --logging.file=./docker/logs/pipelines-ncbi.log
 echo "Imported from NCBI"
 
