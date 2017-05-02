34c7e3123e7d3b3441bcbd3e0da78d43d13ee810
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-05-02 14:23:22 +0100

    expanded transactions

--- models/neo4j/src/main/java/uk/ac/ebi/biosamples/neo/repo/NeoSampleRepositoryImpl.java
+++ models/neo4j/src/main/java/uk/ac/ebi/biosamples/neo/repo/NeoSampleRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.neo4j.ogm.session.Session;
+import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.ebi.biosamples.neo.model.NeoSample;
 
@@ -17,6 +18,7 @@ public class NeoSampleRepositoryImpl implements NeoSampleRepositoryCustom {
 	}
 	
 	@Override
+	@Transactional
 	public NeoSample testNewAccession(String accession) {
 		String cypher = "CREATE (s:Sample {accession:'"+accession+"'}) RETURN s";
 		Map<String,String> parameters = new HashMap<>();
