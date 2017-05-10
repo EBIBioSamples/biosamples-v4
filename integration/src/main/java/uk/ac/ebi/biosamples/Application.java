22b390ff94b0ec6bffc71f23c3915a882579c48c
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-05-10 13:47:21 +0100

    easy adding resttemplate

--- integration/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ integration/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -2,8 +2,10 @@ package uk.ac.ebi.biosamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.client.RestOperations;
 
 @SpringBootApplication
 public class Application {
@@ -18,6 +20,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.exit(SpringApplication.run(Application.class, args));
+	}
+	
+	@Bean
+	public RestOperations getRestOperations(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 	
 }
