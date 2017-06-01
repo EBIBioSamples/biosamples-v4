aa2d86c66892a1b4784001aa1a39595519320fa8
Kerruba <luca.cherubin@gmail.com>
2017-06-01 18:36:03 +0200

    Add sitemap models

---
+++ models/sitemap/src/main/java/uk/ac/ebi/biosamples/model/XmlUrlSet.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright 2015 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *******************************************************************************/
+
+package uk.ac.ebi.biosamples.model;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ *
+ * @author mrelac
+ */
+@XmlAccessorType(value = XmlAccessType.NONE)
+@XmlRootElement(name = "urlset", namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")
+public class XmlUrlSet {
+
+    private final static int MAX_SITEMAP_ENTRIES = 50000;
+
+    @XmlElements({@XmlElement(name = "url", type = XmlUrl.class)})
+    private final List<XmlUrl> xmlUrls = new ArrayList();
+
+    public void addUrl(XmlUrl xmlUrl) {
+
+        final int size = xmlUrls.size();
+
+        // Reservoir sampling to get random entries in the sitemap (this will get them all
+        // eventually, but not exceed the sitemap entry size restriction)
+        if (size >= MAX_SITEMAP_ENTRIES) {
+            // Randomly replace elements in the reservoir with a decreasing probability.
+            int idx = new Double(Math.floor(Math.random() * size)).intValue();
+            if (idx < MAX_SITEMAP_ENTRIES) {
+                xmlUrls.set(idx, xmlUrl);
+            }
+        } else {
+            xmlUrls.add(xmlUrl);
+        }
+    }
+
+
+    public Collection<XmlUrl> getXmlUrls() {
+        return xmlUrls;
+    }
+}
