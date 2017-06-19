82c055a165f46c58b4690b25c03b46111cdbf800
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-06-19 16:46:25 +0100

    tweaks to ise httpclient for cache respectability

--- client/starter/src/main/java/uk/ac/ebi/biosamples/client/BioSamplesAutoConfiguration.java
+++ client/starter/src/main/java/uk/ac/ebi/biosamples/client/BioSamplesAutoConfiguration.java
@@ -3,6 +3,17 @@ package uk.ac.ebi.biosamples.client;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
@@ -12,6 +23,7 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.hal.Jackson2HalModule;
 import org.springframework.hateoas.mvc.TypeConstrainedMappingJackson2HttpMessageConverter;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
@@ -47,23 +59,65 @@ public class BioSamplesAutoConfiguration {
 	@Bean
 	public BioSamplesClient bioSamplesClient(ClientProperties clientProperties, 
 			RestTemplateBuilder restTemplateBuilder, SampleValidator sampleValidator) {		
-		//make sure there is a application/hal+json converter		
-		//traverson will make its own but not if we want to customize the resttemplate in any way (e.g. caching)
 		restTemplateBuilder = restTemplateBuilder.additionalCustomizers(new RestTemplateCustomizer() {
 			public void customize(RestTemplate restTemplate) {
-				List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
 				
+				//use a keep alive strategy to try to make it easier to maintain connections for reuse
+				ConnectionKeepAliveStrategy keepAliveStrategy = new ConnectionKeepAliveStrategy() {
+				    public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+				        HeaderElementIterator it = new BasicHeaderElementIterator
+				            (response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+				        while (it.hasNext()) {
+				            HeaderElement he = it.nextElement();
+				            String param = he.getName();
+				            String value = he.getValue();
+				            if (value != null && param.equalsIgnoreCase
+				               ("timeout")) {
+				                return Long.parseLong(value) * 1000;
+				            }
+				        }
+				        //default to 15s
+				        return 15 * 1000;
+				    }
+				};
+				
+				//set a number of connections to use at once for multiple threads
+				//TODO put this in application.properties
+				PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+				poolingHttpClientConnectionManager.setDefaultMaxPerRoute(16);
+				poolingHttpClientConnectionManager.setMaxTotal(16);
+				
+				
+				//set a timeout limit
+				//TODO put this in application.properties
+				int timeout = 30; //in seconds
+				RequestConfig config = RequestConfig.custom()
+				  .setConnectTimeout(timeout * 1000) //time to establish the connection with the remote host
+				  .setConnectionRequestTimeout(timeout * 1000) //maximum time of inactivity between two data packets
+				  .setSocketTimeout(timeout * 1000).build(); //time to wait for a connection from the connection manager/pool
+				
+				
+				//make the actual client
+				HttpClient httpClient = HttpClientBuilder.create()
+						.setConnectionManager(poolingHttpClientConnectionManager)
+						.setKeepAliveStrategy(keepAliveStrategy)
+						.setDefaultRequestConfig(config)
+						.build();
+				
+				//and wire it into the resttemplate
+		        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+
+				//make sure there is a application/hal+json converter		
+				//traverson will make its own but not if we want to customize the resttemplate in any way (e.g. caching)
+				List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();				
 				ObjectMapper mapper = new ObjectMapper();
 				mapper.registerModule(new Jackson2HalModule());
 				mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
 				MappingJackson2HttpMessageConverter halConverter = new TypeConstrainedMappingJackson2HttpMessageConverter(ResourceSupport.class);
 				halConverter.setObjectMapper(mapper);
 				halConverter.setSupportedMediaTypes(Arrays.asList(MediaTypes.HAL_JSON));
-
 				//make sure this is inserted first
-				converters.add(0, halConverter);
-				
+				converters.add(0, halConverter);				
 				restTemplate.setMessageConverters(converters);
 			}			
 		});
