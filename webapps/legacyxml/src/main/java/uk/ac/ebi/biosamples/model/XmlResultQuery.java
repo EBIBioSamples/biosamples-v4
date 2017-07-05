b10dee64f698a453d11c1e8c84a82f7a3ac61aa5
Kerruba <luca.cherubin@gmail.com>
2017-07-05 12:55:29 +0200

    Started to create the XMLResultQuery using proper Spring notations - JAXB

--- webapps/legacyxml/src/main/java/uk/ac/ebi/biosamples/model/XmlResultQuery.java
+++ webapps/legacyxml/src/main/java/uk/ac/ebi/biosamples/model/XmlResultQuery.java
@@ -1,4 +1,11 @@
 package uk.ac.ebi.biosamples.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "ResultQuery", namespace = "http://www.ebi.ac.uk/biosamples/ResultQuery/1.0")
 public class XmlResultQuery {
+    List<Sample> sampleList;
+
+
 }
