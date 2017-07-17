ceb1eb27c4ab33471446382b04b7fcc6fc5493b5
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-07-17 09:30:33 +0100

    fixes to accessioning

--- webapps/core/src/main/java/uk/ac/ebi/biosamples/service/NeoAccessionService.java
+++ webapps/core/src/main/java/uk/ac/ebi/biosamples/service/NeoAccessionService.java
@@ -1,5 +1,6 @@
-package uk.ac.ebi.biosamples.neo.service;
+package uk.ac.ebi.biosamples.service;
 
+import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.scheduling.annotation.Scheduled;
 import uk.ac.ebi.biosamples.neo.NeoProperties;
+import uk.ac.ebi.biosamples.neo.model.NeoSample;
 import uk.ac.ebi.biosamples.neo.repo.NeoSampleRepository;
 
 /**
@@ -69,17 +71,30 @@ public class NeoAccessionService {
 		return accessionCandidate;
 	}
 
-	@Scheduled(fixedDelay = 100)
+	@Scheduled(fixedDelay = 1000)
 	public void prepareAccessions() {
+		
+		//check that all accessions are still available		
+		Iterator<String> it = accessionCandidateQueue.iterator();
+		while (it.hasNext()) {
+			String accessionCandidate = it.next();
+			NeoSample sample = neoSampleRepository.findOneByAccession(accessionCandidate, 0);
+			if (sample != null) {
+				log.warn("Removing accession "+accessionCandidate+" from queue because now assigned");
+				it.remove();
+			}
+		}
+		
+		//make sure that the queue is full of potential candidates
 		while (accessionCandidateQueue.remainingCapacity() > 0) {
 			log.info("Adding more accessions to queue");
 			String accessionCandidate = neoProperties.getAccessionPrefix() + accessionCandidateCounter;
-			// if the accession already exists, skip it
 			if (neoSampleRepository.findOneByAccession(accessionCandidate,0) != null) {
+				// if the accession already exists, skip it
 				accessionCandidateCounter += 1;
+			} else if (!accessionCandidateQueue.offer(accessionCandidate)) {
 				// if the accession can't be put in the queue at this time
 				// (queue full), stop
-			} else if (!accessionCandidateQueue.offer(accessionCandidate)) {
 				return;
 			} else {
 				//put it into the queue, move on to next
