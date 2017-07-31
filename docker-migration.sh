d9aa858f9ed7f342635f5f276283b50cb09ff76d
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-07-31 09:16:27 +0100

    improving migration script

--- docker-migration.sh
+++ docker-migration.sh
@@ -6,9 +6,7 @@ set -e
 source docker-env.sh
 
 #start up the agents
-#docker-compose up -d biosamples-agents-solr biosamples-agents-neo4j biosamples-agents-curation
 docker-compose up -d biosamples-agents-solr biosamples-agents-curation
-#docker-compose scale biosamples-agents-solr=1 biosamples-agents-neo4j=5
 
 #import from NCBI
 echo "Importing from NCBI"
@@ -18,54 +16,63 @@ ARGS="$ARGS --biosamples.ncbi.file=/home/faulcon/Desktop/biosample_set.xml.gz"
 ARGS="$ARGS --logging.file=./docker/logs/pipelines-ncbi.log"
 ARGS="$ARGS --biosamples.threadcount.max=16"
 #wget -O /home/faulcon/Desktop/biosample_set.xml.gz http://ftp.ncbi.nih.gov/biosample/biosample_set.xml.gz 
-time java -jar pipelines/ncbi/target/pipelines-ncbi-4.0.0-SNAPSHOT.jar $ARGS
+time java -jar pipelines/ncbi/target/pipelines-ncbi-4.0.0-SNAPSHOT.jar $ARGS > /dev/null
 echo "Imported from NCBI"
 
 #pre-assign existing biosample accessions FIRST
+echo "Importing accessions"
+date
 ARGS=
 ARGS="$ARGS --biosamples.client.uri=http://localhost:8081/biosamples/beta" 
 ARGS="$ARGS --logging.file=./docker/logs/pipelines-accession.log"
 ARGS="$ARGS --biosamples.threadcount.max=16"
-time java -jar pipelines/accession/target/pipelines-accession-4.0.0-SNAPSHOT.jar $ARGS
+time java -jar pipelines/accession/target/pipelines-accession-4.0.0-SNAPSHOT.jar $ARGS > /dev/null
+echo "Imported accessions"
+date
 
-#import from ena
 echo "Importing from ENA"
+date
 ARGS=
 ARGS="$ARGS --biosamples.client.uri=http://localhost:8081/biosamples/beta" 
 ARGS="$ARGS --logging.file=./docker/logs/pipelines-ena.log"
 ARGS="$ARGS --biosamples.threadcount.max=16"
-time java -jar pipelines/ena/target/pipelines-ena-4.0.0-SNAPSHOT.jar $ARGS
-#time java -jar pipelines/ena/target/pipelines-ena-4.0.0-SNAPSHOT.jar --from=2017-07-10 --to=2017-07-30 $ARGS
+time java -jar pipelines/ena/target/pipelines-ena-4.0.0-SNAPSHOT.jar $ARGS > /dev/null
 echo "Imported from ENA"
+date
 
-#import sampletab submissions
 echo "Importing SampleTab submissions"
-#export SUBS_HOME=/home/faulcon/Desktop/submissions/GSB
-#rsync -zarv --prune-empty-dirs --include="*/" --include="sampletab.toload.txt" --exclude="*" ebi-cli.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/GSB/ $SUBS_HOME
-#ls $SUBS_HOME/GSB-*/sampletab.toload.txt | xargs -n 1 -P 4 -I {} curl -X POST -H "Content-Type: text/plain" --data-binary @{} http://localhost:8082/biosamples/beta/sampletab/v4
+date
+export SUBS_HOME=/home/faulcon/Desktop/submissions/GSB
+#rsync -zarv --delete --prune-empty-dirs --include="*/" --include="sampletab.toload.txt" --exclude="*" ebi-cli.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/GSB/ $SUBS_HOME
 ARGS=
 ARGS="$ARGS --biosamples.sampletab.uri=http://localhost:8082/biosamples/beta/sampletab/v4" 
 ARGS="$ARGS --biosamples.sampletab.path=/home/faulcon/Desktop/submissions/GSB" 
 ARGS="$ARGS --logging.file=./docker/logs/pipelines-sampletab.gsb.log"
-time java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar $ARGS
+ARGS="$ARGS --biosamples.threadcount.max=16"
+time java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar $ARGS > /dev/null
 echo "Imported SampleTab submissions"
+date
 
-#import from arrayexpress
 echo "Importing ArrayExpress submissions"
+date
 export SUBS_HOME=/home/faulcon/Desktop/submissions/GAE
-#rsync -zarv --prune-empty-dirs --include="*/" --include="sampletab.toload.txt" --exclude="*" ebi-cli.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/ae/ $SUBS_HOME
-#ls $SUBS_HOME/GAE-*/sampletab.toload.txt | xargs -n 1 -P 4 -I {} curl -X POST -H "Content-Type: text/plain" --data-binary @{} http://localhost:8082/biosamples/beta/sampletab/v4
+#rsync -zarv --delete --prune-empty-dirs --include="*/" --include="sampletab.toload.txt" --exclude="*" ebi-cli.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/ae/ $SUBS_HOME
 ARGS=
 ARGS="$ARGS --biosamples.sampletab.uri=http://localhost:8082/biosamples/beta/sampletab/v4" 
 ARGS="$ARGS --biosamples.sampletab.path=/home/faulcon/Desktop/submissions/GAE" 
 ARGS="$ARGS --logging.file=./docker/logs/pipelines-sampletab.gae.log"
-time java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar $ARGS
+ARGS="$ARGS --biosamples.threadcount.max=16"
+time java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar $ARGS > /dev/null
 echo "Imported ArrayExpress submissions"
+date
 
 
 echo "Applying curation"
+date
 ARGS=
 ARGS="$ARGS --biosamples.client.uri=http://localhost:8081/biosamples/beta" 
 ARGS="$ARGS --logging.file=./docker/logs/pipelines-curation.log"
-#time java -jar pipelines/curation/target/pipelines-curation-4.0.0-SNAPSHOT.jar $ARGS
+ARGS="$ARGS --biosamples.threadcount.max=16"
+time java -jar pipelines/curation/target/pipelines-curation-4.0.0-SNAPSHOT.jar $ARGS > /dev/null
 echo "Applied curation"
+date
