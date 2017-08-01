5df8963f7bc193d947b3f5ff7e185a6cc101b486
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-08-01 09:36:53 +0100

    make iterable eagre next page

--- client/client/src/main/java/uk/ac/ebi/biosamples/client/ClientProperties.java
+++ client/client/src/main/java/uk/ac/ebi/biosamples/client/ClientProperties.java
@@ -11,7 +11,14 @@ public class ClientProperties {
 	@Value("${biosamples.client.uri:http://localhost:8081}")
 	private URI biosamplesClientUri;
 
+	@Value("${biosamples.client.pagesize:1000}")
+	private int biosamplesClientPagesize;
+
 	public URI getBiosamplesClientUri() {
 		return biosamplesClientUri;
+	}
+	
+	public int getBiosamplesClientPagesize() {
+		return biosamplesClientPagesize;
 	}
 }
