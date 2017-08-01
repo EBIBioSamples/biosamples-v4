676eef8115376493d3ce610bc49dcb01413cf11f
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-08-01 13:32:05 +0100

    fix to better handle dockerized logging

--- docker-webapp.sh
+++ docker-webapp.sh
@@ -23,9 +23,10 @@ then
 	docker volume ls -q | grep mongo_data | xargs docker volume rm
 	docker volume ls -q | grep solr_data | xargs docker volume rm
 	docker volume ls -q | grep rabbitmq_data | xargs docker volume rm
+	docker volume ls -q | grep logs | xargs docker volume rm
 
-        echo "Cleaning logs"
-        rm -rf docker/logs/*.log docker/logs/*.log.* docker/logs/neo4j/*.log
+#        echo "Cleaning logs"
+#        rm -rf docker/logs/*.log docker/logs/*.log.* docker/logs/neo4j/*.log
 
 #remove any images, in case of out-of-date or corrupt images
 #docker images -q | xargs -r docker rmi
