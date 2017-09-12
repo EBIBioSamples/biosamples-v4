47ba709da1883113e15363193962b73b610e1a35
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-09-12 09:35:34 +0100

    expanded logging

--- pipelines/ena/src/main/java/uk/ac/ebi/biosamples/ena/NcbiCallable.java
+++ pipelines/ena/src/main/java/uk/ac/ebi/biosamples/ena/NcbiCallable.java
@@ -3,12 +3,16 @@ package uk.ac.ebi.biosamples.ena;
 import java.util.Collections;
 import java.util.concurrent.Callable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import uk.ac.ebi.biosamples.client.BioSamplesClient;
 import uk.ac.ebi.biosamples.model.Curation;
 import uk.ac.ebi.biosamples.model.ExternalReference;
 
 public class NcbiCallable implements Callable<Void> {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
 	
 	private final String accession;
 	private final BioSamplesClient bioSamplesClient;
@@ -20,9 +24,11 @@ public class NcbiCallable implements Callable<Void> {
 	
 	@Override
 	public Void call() throws Exception {
+		log.info("HANDLING " + accession);
 		ExternalReference exRef = ExternalReference.build("https://www.ebi.ac.uk/ena/data/view/"+accession);
 		Curation curation = Curation.build(null, null, null, Collections.singleton(exRef));
 		bioSamplesClient.persistCuration(accession, curation);
+		log.info("HANDLED " + accession);
 		return null;
 	}
 
