15707d5a3f00e60966aab70802eb4255dbfa8393
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-09-25 14:00:51 +0100

    removed output redirection

--- docker-migration.sh
+++ docker-migration.sh
@@ -8,10 +8,11 @@ source docker-env.sh
 #start up the agents
 docker-compose up -d biosamples-agents-solr
 
-time docker-compose up biosamples-pipelines-ncbi > /dev/null
+time docker-compose up biosamples-pipelines-ncbi
+
 #pre-assign existing biosample accessions FIRST
-time docker-compose up biosamples-pipelines-accession > /dev/null
-time docker-compose up biosamples-pipelines-ena > /dev/null 
-time docker-compose up biosamples-pipelines-sampletab > /dev/null
+time docker-compose up biosamples-pipelines-accession
+time docker-compose up biosamples-pipelines-ena 
+time docker-compose up biosamples-pipelines-sampletab
  
 #time docker-compose up biosamples-pipelines-curation > /dev/null
