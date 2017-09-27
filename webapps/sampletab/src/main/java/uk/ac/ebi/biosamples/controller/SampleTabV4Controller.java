463327bfdcde96ce4952ff6e26d8b65f39c23c7f
Luca Cherubin <luca.cherubin@gmail.com>
2017-09-27 10:58:57 +0100

    Change user domain for submission through SampleTab

--- webapps/sampletab/src/main/java/uk/ac/ebi/biosamples/controller/SampleTabV4Controller.java
+++ webapps/sampletab/src/main/java/uk/ac/ebi/biosamples/controller/SampleTabV4Controller.java
@@ -1,34 +1,26 @@
 package uk.ac.ebi.biosamples.controller;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.sql.Date;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.mged.magetab.error.ErrorItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import uk.ac.ebi.arrayexpress2.magetab.exception.ParseException;
 import uk.ac.ebi.arrayexpress2.magetab.listener.ErrorItemListener;
 import uk.ac.ebi.arrayexpress2.sampletab.datamodel.SampleData;
 import uk.ac.ebi.arrayexpress2.sampletab.parser.SampleTabParser;
 import uk.ac.ebi.biosamples.service.SampleTabService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class SampleTabV4Controller {
@@ -92,7 +84,7 @@ public class SampleTabV4Controller {
                 
 
         //TODO do AAP domain property
-        sampleTabService.saveSampleTab(sampledata, "aap-users-domain", jwt, setUpdateDate);
+        sampleTabService.saveSampleTab(sampledata, "self.BiosampleIntegrationTest", jwt, setUpdateDate);
         
         return ResponseEntity.ok("");
 	}
