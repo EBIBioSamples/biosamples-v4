6ec179f549f8adf03fd849cfd3347363f2f83853
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-10-03 10:19:30 +0100

    fix sql

--- webapps/sampletab/src/main/java/uk/ac/ebi/biosamples/service/ApiKeyService.java
+++ webapps/sampletab/src/main/java/uk/ac/ebi/biosamples/service/ApiKeyService.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.biosamples.service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,6 +25,21 @@ public class ApiKeyService {
 	
 	public Optional<String> getDomainForApiKey(String apiKey) throws DataAccessException {
 		log.info("getting domain for apikey "+apiKey);
-		return Optional.ofNullable(jdbcTemplate.queryForObject("SELECT AAPDOMAIN FROM USERS WHERE LIKE APIKEY ?", String.class, apiKey));
+
+		String stm = "SELECT AAPDOMAIN FROM USERS WHERE APIKEY LIKE ?";
+    	List<String> results = jdbcTemplate.query(stm, new SingleStringRowMapper(), apiKey);
+    	if (results.size() == 0) {
+    		return Optional.empty();
+    	} else {
+    		return Optional.of(results.get(0));
+    	}
 	}
+
+	protected class SingleStringRowMapper implements RowMapper<String>
+	{
+		public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return rs.getString(1);
+		}
+	}
+
 }
