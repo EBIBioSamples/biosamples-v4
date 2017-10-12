3c35117dd4048984f33aaf16691a6c7793d05be9
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-10-12 13:52:39 +0100

    tweak to output in order

--- integration/src/main/java/uk/ac/ebi/biosamples/migration/AccessionQueueBothCallable.java
+++ integration/src/main/java/uk/ac/ebi/biosamples/migration/AccessionQueueBothCallable.java
@@ -3,8 +3,10 @@ package uk.ac.ebi.biosamples.migration;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
@@ -14,6 +16,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 public class AccessionQueueBothCallable implements Callable<Void> {
@@ -92,20 +95,24 @@ public class AccessionQueueBothCallable implements Callable<Void> {
 		
 		Set<String> newOnly = Sets.difference(newSet, oldSet);
 		Set<String> oldOnly = Sets.difference(oldSet, newSet);
-		log.info("Samples only in new "+newOnly.size());
-		log.info("Samples only in old "+oldOnly.size());
+		List<String> newOnlyList = Lists.newArrayList(newOnly);
+		Collections.sort(newOnlyList);
+		List<String> oldOnlyList = Lists.newArrayList(oldOnly);
+		Collections.sort(oldOnlyList);
+		log.info("Samples only in new "+newOnlyList.size());
+		log.info("Samples only in old "+oldOnlyList.size());
 
 		int i;
 		Iterator<String> accIt;
 		
-		accIt = newOnly.iterator();
+		accIt = newOnlyList.iterator();
 		i = 0;
 		while (accIt.hasNext()) {
 			log.info("Sample only in new "+accIt.next());
 			i++;
 		}
 		
-		accIt = oldOnly.iterator();
+		accIt = oldOnlyList.iterator();
 		i = 0;
 		while (accIt.hasNext()) {
 			log.info("Sample only in old "+accIt.next());
