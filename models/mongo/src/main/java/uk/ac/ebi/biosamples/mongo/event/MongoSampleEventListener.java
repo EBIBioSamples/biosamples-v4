2b2bb90df878d602f650cb7d132d2ff4d9df0de7
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-10-26 09:46:01 +0100

    fix import package error

--- models/mongo/src/main/java/uk/ac/ebi/biosamples/mongo/event/MongoSampleEventListener.java
+++ models/mongo/src/main/java/uk/ac/ebi/biosamples/mongo/event/MongoSampleEventListener.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.event.AfterConvertEvent;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
 
-import com.mongodb.DuplicateKeyException;
+import org.springframework.dao.DuplicateKeyException;
 
 import uk.ac.ebi.biosamples.mongo.model.MongoExternalReference;
 import uk.ac.ebi.biosamples.mongo.model.MongoRelationship;
