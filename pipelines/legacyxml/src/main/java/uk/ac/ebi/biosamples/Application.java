4fdcc92b542df27235337ac088322af2b8c8e7b5
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-11-20 15:34:04 +0000

    remove resttemplate override to allow client config to apply

--- pipelines/legacyxml/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ pipelines/legacyxml/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -1,5 +1,8 @@
 package uk.ac.ebi.biosamples;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.http.HeaderElement;
 import org.apache.http.HeaderElementIterator;
 import org.apache.http.HttpResponse;
@@ -18,10 +21,19 @@ import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.hal.Jackson2HalModule;
+import org.springframework.hateoas.mvc.TypeConstrainedMappingJackson2HttpMessageConverter;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @EnableCaching(proxyTargetClass = true)
@@ -34,70 +46,6 @@ public class Application {
 	@Bean
 	public PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
-	}
-
-	
-	@Bean
-	public RestTemplateCustomizer restTemplateCustomizer() {
-		return new RestTemplateCustomizer() {
-			public void customize(RestTemplate restTemplate) {
-				
-				//use a keep alive strategy to try to make it easier to maintain connections for reuse
-				ConnectionKeepAliveStrategy keepAliveStrategy = new ConnectionKeepAliveStrategy() {
-				    public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
-				    	
-				    	//check if there is a non-standard keep alive header present
-				        HeaderElementIterator it = new BasicHeaderElementIterator
-				            (response.headerIterator(HTTP.CONN_KEEP_ALIVE));
-				        while (it.hasNext()) {
-				            HeaderElement he = it.nextElement();
-				            String param = he.getName();
-				            String value = he.getValue();
-				            if (value != null && param.equalsIgnoreCase
-				               ("timeout")) {
-				                return Long.parseLong(value) * 1000;
-				            }
-				        }
-				        //default to 15s if no header
-				        return 15 * 1000;
-				    }
-				};
-				
-				//set a number of connections to use at once for multiple threads
-				//TODO put this in application.properties
-				PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
-				poolingHttpClientConnectionManager.setDefaultMaxPerRoute(64);
-				poolingHttpClientConnectionManager.setMaxTotal(64);
-				
-				//set a local cache for cacheable responses
-				CacheConfig cacheConfig = CacheConfig.custom()
-				        .setMaxCacheEntries(1024)
-				        .setMaxObjectSize(1024*1024) //max size of 1Mb
-				        //number of entries x size of entries = 1Gb total cache size
-				        .setSharedCache(false) //act like a browser cache not a middle-hop cache
-				        .build();
-				
-				//set a timeout limit
-				//TODO put this in application.properties
-				int timeout = 600; //in seconds
-				RequestConfig config = RequestConfig.custom()
-				  .setConnectTimeout(timeout * 1000) //time to establish the connection with the remote host
-				  .setConnectionRequestTimeout(timeout * 1000) //maximum time of inactivity between two data packets
-				  .setSocketTimeout(timeout * 1000).build(); //time to wait for a connection from the connection manager/pool
-				
-				
-				//make the actual client
-				HttpClient httpClient = CachingHttpClientBuilder.create()
-						.setCacheConfig(cacheConfig)
-						.setConnectionManager(poolingHttpClientConnectionManager)
-						.setKeepAliveStrategy(keepAliveStrategy)
-						.setDefaultRequestConfig(config)
-						.build();
-				
-				//and wire it into the resttemplate
-		        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
-			}
-		};
 	}
 	
 	public static void main(String[] args) {
