d693e745afd3f16522857c104f68c0da960543f4
Adam Faulconbridge <afaulconbridge@googlemail.com>
2017-12-05 11:42:08 +0000

    update migration script to use legacyxml pipeline

--- docker-migration-legacy.sh
+++ docker-migration-legacy.sh
@@ -8,12 +8,6 @@ source docker-env.sh
 #start up the agents
 docker-compose up -d biosamples-agents-solr
 
-#rsync -v -r --delete --include="*/" --include="sampletab.txt" --exclude="*" faulcon@beans.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/GSB /home/faulcon/Desktop/submissions
-#rsync -v -r --delete --include="*/" --include="sampletab.txt" --exclude="*" faulcon@beans.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/ae /home/faulcon/Desktop/submissions
-#rsync -v -r --delete --include="*/" --include="sampletab.txt" --exclude="*" faulcon@beans.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/sra /home/faulcon/Desktop/submissions
-#rsync -v -r --delete --include="*/" --include="sampletab.txt" --exclude="*" faulcon@beans.ebi.ac.uk:/ebi/microarray/home/biosamples/production/data/GNC /home/faulcon/Desktop/submissions
+export BIOSAMPLES_CLIENT_URI="http://biosamples-webapps-core:8081/biosamples/beta"
 
-java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar -Xverify:none --biosamples.sampletab.uri="http://localhost:8082/biosamples/beta/sampletab/v4?setupdatedate" --biosamples.sampletab.path=/home/faulcon/Desktop/submissions/GSB
-java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar -Xverify:none --biosamples.sampletab.uri="http://localhost:8082/biosamples/beta/sampletab/v4?setupdatedate" --biosamples.sampletab.path=/home/faulcon/Desktop/submissions/ae
-java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar -Xverify:none --biosamples.sampletab.uri="http://localhost:8082/biosamples/beta/sampletab/v4?setupdatedate" --biosamples.sampletab.path=/home/faulcon/Desktop/submissions/sra
-java -jar pipelines/sampletab/target/pipelines-sampletab-4.0.0-SNAPSHOT.jar -Xverify:none --biosamples.sampletab.uri="http://localhost:8082/biosamples/beta/sampletab/v4?setupdatedate" --biosamples.sampletab.path=/home/faulcon/Desktop/submissions/GNC
+java -jar pipelines/pipelines-legacyxml/target/pipelines-legacyxml-4.0.0-SNAPSHOT.jar import ./biosamples.beans.xml.gz ./ 
