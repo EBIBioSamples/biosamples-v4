36dd03fd62f1162d52b738b60489d1040a825a63
Luca Cherubin <luca.cherubin@gmail.com>
2017-12-21 16:43:38 +0100

    Reformat sampletab webpage

---
+++ webapps/sampletab/src/main/java/uk/ac/ebi/biosamples/controller/SubmissionServicesController.java
@@ -0,0 +1,27 @@
+package uk.ac.ebi.biosamples.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SubmissionServicesController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/submission")
+    public String submissionService() {
+        return "submission";
+    }
+
+    @RequestMapping("/validation")
+    public String validationService() {
+        return "validation";
+    }
+    @RequestMapping("/accession")
+    public String accessionService() {
+        return "accession";
+    }
+}
