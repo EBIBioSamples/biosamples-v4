a87e500f6847c0e118dca0bdf833622551494ae1
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-01-29 15:31:53 +0000

    update docker integration test script to setup mongo with sampletab api key

--- docker-integration.sh
+++ docker-integration.sh
@@ -3,23 +3,13 @@ set -e
 
 ./docker-webapp.sh --clean
 
-#to pass along username/password set them as environment variables like so:
-#export biosamples_client_aap_username=
-#export biosamples_client_aap_password=
-#export biosamples_legacyapikey=
-
-#setup arguments to use for tests
-ARGS=
-#ARGS="$ARGS --biosamples.client.uri=http://localhost:8081/biosamples/beta" 
-#ARGS="$ARGS --biosamples.submissionuri.sampletab=http://localhost:8082/biosamples/beta/sampletab"
-#ARGS="$ARGS --biosamples.legacyxml.uri=http://localhost:8083/biosamples/beta/xml"
-#ARGS="$ARGS --spring.profiles.active=default"
+docker-compose run --rm mongo mongo --eval 'db.mongoSampleTabApiKey.insert({"_id" : "fooqwerty", "_class" : "uk.ac.ebi.biosamples.mongo.model.MongoSampleTabApiKey", "userName" : "BioSamples", "publicEmail" : "", "publicUrl" : "", "contactName" : "", "contactEmail" : "", "aapDomain" : "123456789abcdef" });' mongo:27017
 
 for X in 1 2 3 4 5
 do
   #java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.0-SNAPSHOT.jar --biosamples.agent.solr.stayalive=false --biosamples.agent.solr.queuetime=500
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.2.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.2.jar --biosamples.agent.solr.stayalive=false --biosamples.agent.solr.queuetime=500
 done
 
 #leave the agent up at the end
