e0379595551e576dfdcfea4c7f2725f852d65dd7
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-01-30 11:52:19 +0000

    add /groups to /samples redirect BSD-1026

--- webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/SampleHtmlController.java
+++ webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/SampleHtmlController.java
@@ -23,10 +23,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -311,4 +313,16 @@ public class SampleHtmlController {
 
 		return "sample";
 	}
+	
+
+    @GetMapping("/groups/{accession}")
+    public String groupsAccession(@PathVariable String accession) {
+        return "redirect:/samples/"+accession;
+    }
+    
+    @GetMapping("/groups")
+    public String groups() {
+        return "redirect:/samples";
+    }
+	
 }
