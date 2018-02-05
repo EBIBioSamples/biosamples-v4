85b727e80ba8d77ddf98f8e27a42d4c0a13a8348
Luca Cherubin <luca.cherubin@gmail.com>
2018-02-05 15:36:23 +0000

    Fixes bug BSD-1040: Webpages have no title

--- docker-test.sh
+++ docker-test.sh
@@ -5,8 +5,8 @@ set -e
 for X in "$@"
 do
   #java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.0-SNAPSHOT.jar --biosamples.agent.solr.stayalive=false --biosamples.agent.solr.queuetime=500
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.3-SNAPSHOT.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.3-SNAPSHOT.jar --biosamples.agent.solr.stayalive=false --biosamples.agent.solr.queuetime=500
 done
 
 echo "Successfully completed"
