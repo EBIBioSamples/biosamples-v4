6838b1b1cbb70bc2b782c9d7d8587c9efe8587b7
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-02-06 10:29:34 +0000

    add integration testing for facet and filter following on paging

--- integration/src/main/java/uk/ac/ebi/biosamples/BigIntegration.java
+++ integration/src/main/java/uk/ac/ebi/biosamples/BigIntegration.java
@@ -1,7 +1,9 @@
 package uk.ac.ebi.biosamples;
 
+import java.net.URI;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.SortedSet;
@@ -9,10 +11,28 @@ import java.util.TreeSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.hal.Jackson2HalModule;
+import org.springframework.hateoas.mvc.TypeConstrainedMappingJackson2HttpMessageConverter;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 
 import uk.ac.ebi.biosamples.client.BioSamplesClient;
@@ -25,13 +45,33 @@ import uk.ac.ebi.biosamples.model.Sample;
 public class BigIntegration extends AbstractIntegration {
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
+	private final RestOperations restOperations;
+	private final BioSamplesProperties bioSamplesProperties;
 	
 	//must be over 1000
 	private final int noSamples = 5000;
 	
 	
-	public BigIntegration(BioSamplesClient client) {
+	public BigIntegration(BioSamplesClient client, RestTemplateBuilder restTemplateBuilder, BioSamplesProperties bioSamplesProperties) {
 		super(client);
+		RestTemplate restTemplate = restTemplateBuilder.build();
+		
+		//make sure there is a application/hal+json converter		
+		//traverson will make its own but not if we want to customize the resttemplate in any way (e.g. caching)
+		List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();				
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new Jackson2HalModule());
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		MappingJackson2HttpMessageConverter halConverter = new TypeConstrainedMappingJackson2HttpMessageConverter(ResourceSupport.class);
+		halConverter.setObjectMapper(mapper);
+		halConverter.setSupportedMediaTypes(Arrays.asList(MediaTypes.HAL_JSON));
+		//make sure this is inserted first
+		converters.add(0, halConverter);				
+		restTemplate.setMessageConverters(converters);
+		
+		this.restOperations = restTemplate;
+		
+		this.bioSamplesProperties = bioSamplesProperties;
 	}
 
 	@Override
@@ -93,7 +133,7 @@ public class BigIntegration extends AbstractIntegration {
 
 		startTime = System.nanoTime();
 		for (Resource<Sample> sample : client.fetchSampleResourceAll()) {
-			
+			//do nothing
 		}
 		endTime = System.nanoTime();
 		elapsedMs = (int) ((endTime-startTime)/1000000l);
@@ -101,6 +141,39 @@ public class BigIntegration extends AbstractIntegration {
 			throw new RuntimeException("Took more than 5000ms to fetch all samples ("+elapsedMs+"ms)");			
 		}
 
+		//TODO check HAL links for search term and facets are persistent over paging etc
+		
+		
+		URI uri = UriComponentsBuilder
+				.fromUri(bioSamplesProperties.getBiosamplesClientUri())
+				.pathSegment("samples")
+				.queryParam("text", "Sample")
+				.queryParam("filter", "attr:organism:Homo sapiens")
+				.build().encode().toUri();
+		log.info("checking HAL links on "+uri);
+		ResponseEntity<PagedResources<Resource<Sample>>> responseEntity = restOperations.exchange(
+				RequestEntity.get(uri).accept(MediaTypes.HAL_JSON).build(), 
+				new ParameterizedTypeReference<PagedResources<Resource<Sample>>>(){});
+		
+		PagedResources<Resource<Sample>> page = responseEntity.getBody();
+		log.info("looking for links in "+page);
+		for (Link link : page.getLinks()) {
+			log.info("Found link "+link);
+		}
+		Link firstLink = page.getLink("first");
+		UriComponents firstLinkUriComponents = UriComponentsBuilder.fromUriString(firstLink.getHref())
+				.build();
+		
+		String firstFilter = firstLinkUriComponents.getQueryParams().get("filter").get(0);
+		if (!"attr:organism:Homo%20sapiens".equals(firstFilter)) {
+			throw new RuntimeException("Expected first relationship URL to include parameter filter with value 'attr:organism:Homo sapiens' but got '"
+					+firstFilter+"'");
+		}
+		String firstText = firstLinkUriComponents.getQueryParams().get("text").get(0);
+		if (!"Sample".equals(firstText)) {
+			throw new RuntimeException("Expected first relationship URL to include parameter text with value 'Sample' but got '"
+					+firstText+"'");
+		}
 	}
 
 	@Override
