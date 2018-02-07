1c298a8c4f9f9736cc32732f3e15ce36457aeb65
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-02-07 16:55:31 +0000

    Add configuration to solr page processing

--- properties/src/main/java/uk/ac/ebi/biosamples/BioSamplesProperties.java
+++ properties/src/main/java/uk/ac/ebi/biosamples/BioSamplesProperties.java
@@ -59,6 +59,12 @@ public class BioSamplesProperties {
 	@Value("${biosamples.ols:http://wwwdev.ebi.ac.uk/ols}")
 	private String ols;
 
+	@Value("${biosamples.webapp.core.page.threadcount:64}")
+	private int webappCorePageThreadCount;
+
+	@Value("${biosamples.webapp.core.page.threadcount.max:128}")
+	private int webappCorePageThreadCountMax;
+
 	
 	public URI getBiosamplesClientUri() {
 		return biosamplesClientUri;
@@ -123,4 +129,13 @@ public class BioSamplesProperties {
 	public String getOls() {
 		return ols;
 	}
+
+	public int getBiosamplesCorePageThreadCount() {
+		return webappCorePageThreadCount;
+	}
+
+	public int getBiosamplesCorePageThreadCountMax() {
+		return webappCorePageThreadCountMax;
+	}
+	
 }
