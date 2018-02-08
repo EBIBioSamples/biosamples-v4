bb066acf95851952e96aa378806d85a57685f0b7
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-02-08 14:07:28 +0000

    added first version of json dump based importer

---
+++ pipelines/legacyxml/src/main/java/uk/ac/ebi/biosamples/legacyxml/ClientImporter.java
@@ -0,0 +1,86 @@
+package uk.ac.ebi.biosamples.legacyxml;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.zip.GZIPInputStream;
+
+import org.dom4j.Document;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.hateoas.Resource;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import uk.ac.ebi.biosamples.client.BioSamplesClient;
+import uk.ac.ebi.biosamples.legacyxml.ImportJsonRunner.ImportJsonCallback;
+import uk.ac.ebi.biosamples.model.Sample;
+import uk.ac.ebi.biosamples.service.SampleToXmlConverter;
+import uk.ac.ebi.biosamples.utils.AdaptiveThreadPoolExecutor;
+import uk.ac.ebi.biosamples.utils.ThreadUtils;
+import uk.ac.ebi.biosamples.utils.JsonFragmenter.JsonCallback;
+
+@Component
+public class ClientImporter implements ApplicationRunner {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	private final BioSamplesClient client;
+	private final ObjectMapper objectMapper;
+	
+	public ClientImporter(BioSamplesClient client, 
+			ObjectMapper objectMapper) {
+		this.client = client;
+		this.objectMapper = objectMapper;
+	}
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		if (!"clientimport".equals(args.getNonOptionArgs().get(0))) {
+			return;
+		}
+
+		Path inputJsonPath = Paths.get(args.getNonOptionArgs().get(1));
+	    Map<String, Future<Resource<Sample>>> futures = new HashMap<>();
+
+		try (InputStream inputStream = new GZIPInputStream(new BufferedInputStream(Files.newInputStream(inputJsonPath)))) {
+
+		    JsonParser parser = objectMapper.getFactory().createParser(inputStream);
+		    if (parser.nextToken() != JsonToken.START_ARRAY) {
+		    	throw new IllegalStateException("A JSON array was expected");
+			}
+		    while (parser.nextToken() == JsonToken.START_ARRAY) {
+		    	log.info("skipping start array");
+		    }
+		    Iterator<Sample> it = objectMapper.readerFor(Sample.class).readValues(parser);
+			while (it.hasNext()) {
+				Sample sample = it.next();
+				Future<Resource<Sample>> future = client.persistSampleResourceAsync(sample);
+				futures.put(sample.getAccession(), future);
+				ThreadUtils.checkFutures(futures, 1000);
+			}
+		} finally {
+			ThreadUtils.checkFutures(futures, 0);
+		}
+	}
+}
