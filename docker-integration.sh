8891ffbbff142405cafd9c59822e7b7f3940d975
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-02-08 13:10:18 +0000

    update integration version

--- docker-integration.sh
+++ docker-integration.sh
@@ -11,7 +11,7 @@ docker-compose up -d biosamples-agents-solr
 for X in 1 2 3 4 5
 do
   #java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.4.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.4-SNAPSHOT.jar --phase=$X $ARGS $@
   sleep 10 #solr is configured to commit every 5 seconds
 done
 
