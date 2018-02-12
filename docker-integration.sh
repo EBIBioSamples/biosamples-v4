1b58e2244f74be3c18c61fd081950679683226fb
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-02-12 11:55:58 +0000

    prepare for 4.0.5-SNAPSHOT

--- docker-integration.sh
+++ docker-integration.sh
@@ -8,8 +8,8 @@ docker-compose run --rm mongo mongo --eval 'db.mongoSampleTabApiKey.insert({"_id
 for X in 1 2 3 4 5
 do
   #java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.4.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.4.jar --biosamples.agent.solr.stayalive=false --biosamples.agent.solr.queuetime=500
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.0.5-SNAPSHOT.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.5-SNAPSHOT.jar --biosamples.agent.solr.stayalive=false --biosamples.agent.solr.queuetime=500
 done
 
 #leave the agent up at the end
