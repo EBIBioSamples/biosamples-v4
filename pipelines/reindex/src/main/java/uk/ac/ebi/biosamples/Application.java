b634604dee003d2b33a68af984493b6298b26b1c
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-03-08 17:12:27 +0000

    added re-index pipeline

---
+++ pipelines/reindex/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.biosamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.exit(SpringApplication.run(Application.class, args));
+	}
+}
