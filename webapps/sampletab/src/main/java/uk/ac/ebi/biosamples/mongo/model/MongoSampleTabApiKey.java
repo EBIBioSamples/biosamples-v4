b2b73159aefdaad0d400abb8ed9dc74455cb4eab
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-05-21 10:44:36 +0100

    added apikey gdpr consent storage

--- webapps/sampletab/src/main/java/uk/ac/ebi/biosamples/mongo/model/MongoSampleTabApiKey.java
+++ webapps/sampletab/src/main/java/uk/ac/ebi/biosamples/mongo/model/MongoSampleTabApiKey.java
@@ -7,16 +7,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class MongoSampleTabApiKey {
 
 	@Id
-	private String apiKey;	
+	private String apiKey;
 	private String userName;
 	private String publicEmail;
 	private String publicUrl;
 	private String contactName;
 	private String contactEmail;
 	private String aapDomain;
-	
+	private String gdprConsentDate;
+	private String gdprConsentVersion;
+
 	private MongoSampleTabApiKey(String apiKey, String userName, String publicEmail, String publicUrl,
-			String contactName, String contactEmail, String aapDomain) {
+			String contactName, String contactEmail, String aapDomain, String gdprConsentDate, String gdprConsentVersion) {
 		super();
 		this.apiKey = apiKey;
 		this.userName = userName;
@@ -25,39 +27,51 @@ public class MongoSampleTabApiKey {
 		this.contactName = contactName;
 		this.contactEmail = contactEmail;
 		this.aapDomain = aapDomain;
+		this.gdprConsentDate = gdprConsentDate;
+		this.gdprConsentVersion = gdprConsentVersion;
 	}
 
 	public String getApiKey() {
 		return apiKey;
 	}
-	
+
 	public String getUserName() {
 		return userName;
 	}
-	
+
 	public String getPublicEmail() {
 		return publicEmail;
 	}
-	
+
 	public String getPublicUrl() {
 		return publicUrl;
 	}
-	
+
 	public String getContactName() {
 		return contactName;
 	}
-	
+
 	public String getContactEmail() {
 		return contactEmail;
 	}
-	
+
 	public String getAapDomain() {
 		return aapDomain;
 	}
-	
-	public static MongoSampleTabApiKey build(String apiKey, String userName, String publicEmail, String publicUrl,
-			String contactName, String contactEmail, String aapDomain) {
-		return new MongoSampleTabApiKey(apiKey, userName, publicEmail, publicUrl, contactName, contactEmail, aapDomain);
+
+	public String getGdprConsentDate() {
+		return gdprConsentDate;
 	}
-	
+
+	public String getGdprConsentVersion() {
+		return gdprConsentVersion;
+	}
+
+	public static MongoSampleTabApiKey build(String apiKey, String userName, String publicEmail, String publicUrl,
+			String contactName, String contactEmail, String aapDomain, String gdprConsentDate,
+			String gdprConsentVersion) {
+		return new MongoSampleTabApiKey(apiKey, userName, publicEmail, publicUrl, contactName, contactEmail, aapDomain,
+				gdprConsentDate, gdprConsentVersion);
+	}
+
 }
