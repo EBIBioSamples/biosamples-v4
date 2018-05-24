c2cd3713c9b77463489dacc4263cba0ceb08fe5b
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-05-24 19:55:15 +0100

    prepare for 4.0.8 GDPR patch

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
   
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.8-SNAPSHOT.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.0.8.jar
 echo "Successfully runned agents"
