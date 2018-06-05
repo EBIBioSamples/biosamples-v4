d830e10ba7d4ca5d37a78252aa013e2d5320ac31
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-06-05 13:12:06 +0100

    improve inverse relationship performance

--- models/mongo/src/main/java/uk/ac/ebi/biosamples/mongo/repo/MongoSampleRepository.java
+++ models/mongo/src/main/java/uk/ac/ebi/biosamples/mongo/repo/MongoSampleRepository.java
@@ -1,8 +1,5 @@
 package uk.ac.ebi.biosamples.mongo.repo;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,7 +11,4 @@ public interface MongoSampleRepository extends MongoRepository<MongoSample, Stri
 	Page<MongoSample> findByExternalReferences_Hash(String urlHash, Pageable pageable);
 
 	Page<MongoSample> findByDomainAndName(String domain, String name, Pageable pageable);
-	
-//	List<MongoSample> findAllByRelationshipsTarget(String target);
-	Stream<MongoSample> findAllByRelationshipsTarget(String target);
 }
