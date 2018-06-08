0163a4468c9343850eb619fa65b3af9482e4c615
Luca Cherubin <luca.cherubin@gmail.com>
2018-06-08 09:12:55 +0100

    Starts moving

---
+++ webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/SampletabHtmlController.java
@@ -0,0 +1,29 @@
+package uk.ac.ebi.biosamples.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sampletab")
+public class SampletabHtmlController {
+
+    @RequestMapping(value = {"", "/"} )
+    public String index(){
+        return "sampletab/index";
+    }
+
+    @RequestMapping("/submission")
+    public String submissionService() {
+        return "sampletab/submission";
+    }
+
+    @RequestMapping("/validation")
+    public String validationService() {
+        return "sampletab/validation";
+    }
+    @RequestMapping("/accession")
+    public String accessionService() {
+        return "sampletab/accession";
+    }
+
+}
