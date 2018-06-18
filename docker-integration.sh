47f6dd842445c119e67db91a5f035be6d98c0fe9
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-06-18 14:24:30 +0100

    reduce integration inter-stage delay

--- docker-integration.sh
+++ docker-integration.sh
@@ -20,7 +20,7 @@ for X in 1 2 3 4 5
 do
   #java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
   docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.1.1-SNAPSHOT.jar --phase=$X $ARGS $@
-  sleep 30 #solr is configured to commit every 5 seconds
+  sleep 10 #solr is configured to commit every 5 seconds
 
 done
 
