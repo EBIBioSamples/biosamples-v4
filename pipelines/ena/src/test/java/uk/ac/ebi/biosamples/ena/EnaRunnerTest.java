a15c43b2b2609737d4edb876e2613bdd6547e5ef
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-07-12 16:34:57 +0100

    Fixed converter when the value is missing the set the value to "unknown" instead of the tag

--- pipelines/ena/src/test/java/uk/ac/ebi/biosamples/ena/EnaRunnerTest.java
+++ pipelines/ena/src/test/java/uk/ac/ebi/biosamples/ena/EnaRunnerTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +12,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
 import uk.ac.ebi.biosamples.Application;
 import uk.ac.ebi.biosamples.BioSamplesProperties;
 import uk.ac.ebi.biosamples.client.BioSamplesClient;
+import uk.ac.ebi.biosamples.model.Curation;
+import uk.ac.ebi.biosamples.model.CurationLink;
 import uk.ac.ebi.biosamples.model.Sample;
 
 import java.io.BufferedWriter;
@@ -24,6 +26,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -74,21 +78,26 @@ public class EnaRunnerTest {
 
         private class MockBioSamplesClient extends BioSamplesClient {
 
-            private BufferedWriter writer;
+            private BufferedWriter samplesWriter;
+
+            private BufferedWriter curationsWriter;
 
             public MockBioSamplesClient() {
                 super(bioSamplesProperties.getBiosamplesClientUri(), restTemplateBuilder, null, null, bioSamplesProperties);
                 try {
-                    writer = Files.newBufferedWriter(Paths.get("output.txt"));
+                    samplesWriter = Files.newBufferedWriter(Paths.get("samples-output.txt"));
+                    curationsWriter = Files.newBufferedWriter(Paths.get("curations-output.txt"));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+
             }
 
             @Override
             public Future<Resource<Sample>> persistSampleResourceAsync(Sample sample, Boolean setUpdateDate, Boolean setFullDetails) {
                 try {
-                    writer.write(sample.toString() + "\n");
+                    samplesWriter.write(sample.toString() + "\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -121,9 +130,27 @@ public class EnaRunnerTest {
             }
 
             @Override
+            public Resource<CurationLink> persistCuration(String accession, Curation curation, String domain) {
+                try {
+                    log.info("Persisting curation " + curation + " on " + accession + " in " + domain);
+                    curationsWriter.write(curation.toString() + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            public Optional<Resource<Sample>> fetchSampleResource(String accession,
+                                                                  Optional<List<String>> curationDomains) throws RestClientException {
+                return Optional.empty();
+            }
+
+            @Override
             public void finalize() {
                 try {
-                    writer.close();
+                    samplesWriter.close();
+                    curationsWriter.close();
                     System.out.println("Closed BufferedWriter in the finalizer");
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -134,6 +161,6 @@ public class EnaRunnerTest {
 
     @Test
     public void run() throws Exception {
-       // enaRunner.run(new DefaultApplicationArguments(new String[]{"from=2018-07-01"}));
+        // enaRunner.run(new DefaultApplicationArguments(new String[]{"from=2018-07-01"}));
     }
 }
