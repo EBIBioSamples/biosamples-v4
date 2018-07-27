b21bbb6f57bd440d43837fac444490670f3f5c1a
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-07-27 14:51:45 +0100

    Added initial live list pipeline for BSD-763

--- pipelines/livelist/src/main/java/uk/ac/ebi/biosamples/livelist/LiveListRunner.java
+++ pipelines/livelist/src/main/java/uk/ac/ebi/biosamples/livelist/LiveListRunner.java
@@ -1,13 +1,60 @@
 package uk.ac.ebi.biosamples.livelist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.hateoas.Resource;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.biosamples.client.BioSamplesClient;
+import uk.ac.ebi.biosamples.model.Sample;
 
-public class LiveListRunner  implements ApplicationRunner {
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.zip.GZIPOutputStream;
+
+@Component
+public class LiveListRunner implements ApplicationRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LiveListRunner.class);
+
+    private final BioSamplesClient bioSamplesClient;
+
+
+    public LiveListRunner(BioSamplesClient bioSamplesClient) {
+        //ensure the client is public
+        if (bioSamplesClient.getPublicClient().isPresent()) {
+            this.bioSamplesClient = bioSamplesClient.getPublicClient().get();
+        } else {
+            this.bioSamplesClient = bioSamplesClient;
+        }
+    }
 
     @Override
-    public void run(ApplicationArguments applicationArguments) throws Exception {
-
+    public void run(ApplicationArguments args) throws Exception {
+        String liveListFilename = args.getNonOptionArgs().get(0);
+        long startTime = System.nanoTime();
+        int sampleCount = 0;
+        try {
+            try (
+                    Writer liveListWriter = args.getOptionValues("gzip") == null
+                            ? new OutputStreamWriter(new FileOutputStream(liveListFilename), "UTF-8")
+                            : new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(liveListFilename)), "UTF-8");
+            ) {
+                for (Resource<Sample> sampleResource : bioSamplesClient.fetchSampleResourceAll()) {
+                    LOGGER.trace("Handling " + sampleResource);
+                    Sample sample = sampleResource.getContent();
+                    liveListWriter.write(LiveListUtils.createLiveListString(sample));
+                    liveListWriter.write("\n");
+                    sampleCount++;
+                }
+            } finally {
+            }
+        } finally {
+        }
+        long elapsed = System.nanoTime() - startTime;
+        LOGGER.info("Live list exported " + sampleCount + " samples in " + (elapsed / 1000000000l) + "s");
     }
 
 }
---
+++ pipelines/livelist/src/test/java/uk/ac/ebi/biosamples/livelist/LiveListUtilsTest.java
@@ -0,0 +1,37 @@
+package uk.ac.ebi.biosamples.livelist;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.biosamples.Application;
+import uk.ac.ebi.biosamples.model.Sample;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static uk.ac.ebi.biosamples.livelist.LiveListUtils.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(Application.class)
+public class LiveListUtilsTest {
+
+    private String sampleJson = "{\"name\":\"MiSeq, Diospyros lotus L8 male flower 23 Apr MeGI promoter\",\"accession\":\"SAMD00046940\",\"domain\":\"123456789abcdef\",\"release\":\"2017-04-06T23:00:00Z\",\"update\":\"2018-07-25T09:28:16.841Z\",\"characteristics\":{\"Organism\":[{\"text\":\"Diospyros lotus\",\"ontologyTerms\":[\"55363\"]}],\"Sex\":[{\"text\":\"male\",\"ontologyTerms\":[\"http://purl.obolibrary.org/obo/PATO_0000384\"]}],\"colection date\":[{\"text\":\"2014-04-23\"}],\"cultivar\":[{\"text\":\"KK-L8\"}],\"model\":[{\"text\":\"Generic\"}],\"organism part\":[{\"text\":\"whole flower\"}],\"package\":[{\"text\":\"Generic.1.0\"}],\"synonym\":[{\"text\":\"TA_Meth-2\"}]},\"externalReferences\":[{\"url\":\"http://www.ebi.ac.uk/ena/data/view/SAMD00046940\"}],\"releaseDate\":\"2017-04-06\",\"updateDate\":\"2018-07-25\"}";
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void given_a_sample_return_expected_live_list_entry() throws IOException {
+        Sample sample = objectMapper.readValue(sampleJson, Sample.class);
+        String liveListString = createLiveListString(sample);
+        assertNotNull(liveListString);
+        assertEquals(5, liveListString.split("\\|").length);
+    }
+
+}
