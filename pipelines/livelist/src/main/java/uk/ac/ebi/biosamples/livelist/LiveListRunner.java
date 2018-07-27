134d497be64004ca814ddb0bc1b4a89bde4cdec1
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-07-27 13:36:13 +0100

    Initial stub for BSD-763

---
+++ pipelines/livelist/src/main/java/uk/ac/ebi/biosamples/livelist/LiveListRunner.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.biosamples.livelist;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class LiveListRunner  implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+
+    }
+
+}
