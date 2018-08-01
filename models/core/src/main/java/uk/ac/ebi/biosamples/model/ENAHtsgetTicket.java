a884768f50856f016e9daeb9c95b857962bac5cf
Dilschat <dilschat@yandex.ru>
2018-08-01 15:30:11 +0300

    code cleaned

--- models/core/src/main/java/uk/ac/ebi/biosamples/model/ENAHtsgetTicket.java
+++ models/core/src/main/java/uk/ac/ebi/biosamples/model/ENAHtsgetTicket.java
@@ -1,10 +1,10 @@
 package uk.ac.ebi.biosamples.model;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
 @JsonDeserialize(using = TicketDeserializer.class)
 public class ENAHtsgetTicket {
 
--- models/core/src/main/java/uk/ac/ebi/biosamples/model/TicketDeserializer.java
+++ models/core/src/main/java/uk/ac/ebi/biosamples/model/TicketDeserializer.java
@@ -1,7 +1,6 @@
 package uk.ac.ebi.biosamples.model;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
