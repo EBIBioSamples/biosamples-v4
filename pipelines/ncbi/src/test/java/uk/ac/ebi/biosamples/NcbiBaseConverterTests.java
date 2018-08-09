cc0acb989028d03788d3a55f8d8937100b46640d
Luca Cherubin <luca.cherubin@gmail.com>
2018-08-09 09:58:26 +0200

    Add further tests

--- pipelines/ncbi/src/test/java/uk/ac/ebi/biosamples/NcbiBaseConverterTests.java
+++ pipelines/ncbi/src/test/java/uk/ac/ebi/biosamples/NcbiBaseConverterTests.java
@@ -1,6 +1,9 @@
 package uk.ac.ebi.biosamples;
 
-import org.dom4j.*;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +16,15 @@ import uk.ac.ebi.biosamples.utils.TaxonomyService;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static org.mockito.Matchers.any;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 public class NcbiBaseConverterTests {
@@ -37,10 +45,10 @@ public class NcbiBaseConverterTests {
     }
 
     @Test
-    public void given_ncbi_biosample_extract_accession_and_alias() {
+    public void given_ncbi_biosample_extract_accession_name_synonym() {
         Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
-        assert(sampleToTest.getAccession().equals("SAMN05246317"));
-        assert(sampleToTest.getName().equals("GF.26.AL.R"));
+        assertEquals(sampleToTest.getAccession(), "SAMN05246317");
+        assertEquals(sampleToTest.getName(), "GF.26.AL.R");
     }
 
     @Test
@@ -48,10 +56,9 @@ public class NcbiBaseConverterTests {
         Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
         Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
                 .filter(attr -> attr.getType().equals("INSDC secondary accession")).findFirst();
-        assert(expectedAttribute.isPresent());
 
         Attribute secondaryAccession = expectedAttribute.get();
-        assert(secondaryAccession.getValue().equals("SRS1524325"));
+        assertEquals(secondaryAccession.getValue(),"SRS1524325");
     }
 
     @Test
@@ -59,10 +66,140 @@ public class NcbiBaseConverterTests {
         Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
         Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
                 .filter(attr -> attr.getType().equals("INSDC center name")).findFirst();
-        assert(expectedAttribute.isPresent());
+        assertTrue(expectedAttribute.isPresent());
 
         Attribute centerName = expectedAttribute.get();
-        assert(centerName.getValue().equals("Lund University"));
+        assertEquals(centerName.getValue(), "Lund University");
+    }
+
+    @Test
+    public void it_extracts_organism_attribute() {
+        Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
+        Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("organism"))
+                .findFirst();
+
+        assertTrue(expectedAttribute.isPresent());
+
+        Attribute organism = expectedAttribute.get();
+        assertEquals(organism.getValue(), "soil metagenome");
+        assertEquals(organism.getIri().first(), "http://purl.obolibrary.org/obo/NCBITaxon_410658");
+    }
+
+    @Test
+    public void it_extracts_description_title() {
+        Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
+        Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("description title"))
+                .findFirst();
+
+        assertTrue(expectedAttribute.isPresent());
+
+        Attribute description = expectedAttribute.get();
+        assertEquals(description.getValue(), "Metagenome or environmental sample from soil metagenome");
+
+    }
+
+    @Test
+    public void it_extracts_attributes() {
+        Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
+        SortedSet<Attribute> sampleAttributes = sampleToTest.getAttributes();
+        List<Attribute> expectedAttributes = Stream.of(
+                Attribute.build("isolation source", "Alseis blackiana roots"),
+                Attribute.build("collection date", "Sep-2012"),
+                Attribute.build("geographic location", "Panama:Gigante_peninsula"),
+                Attribute.build("latitude and longitude", "9.110057 N 79.8434 W"),
+                Attribute.build("Fert_treat", "unfertilized"),
+                Attribute.build("plot", "GF_26")
+        ).collect(Collectors.toList());
+
+
+        Optional<Attribute> attributesNotMatching = expectedAttributes.stream()
+                .filter( attr -> !sampleAttributes.contains(attr)).findAny();
+        assertTrue(!attributesNotMatching.isPresent());
+
+    }
+
+    @Test
+    public void it_extracts_ncbi_submission_model(){
+        Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
+        Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("NCBI submission model"))
+                .findFirst();
+
+        assertTrue(expectedAttribute.isPresent());
+
+        Attribute ncbiSubmissionModel = expectedAttribute.get();
+        assertEquals(ncbiSubmissionModel.getValue(), "Metagenome or environmental");
+
+    }
+
+    @Test
+    public void it_extracts_ncbi_submission_package(){
+        Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
+        Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("NCBI submission package"))
+                .findFirst();
+
+        assertTrue(expectedAttribute.isPresent());
+
+        Attribute ncbiSubmissionPackage = expectedAttribute.get();
+        assertEquals(ncbiSubmissionPackage.getValue(), "Metagenome.environmental.2.0");
+
+    }
+
+    @Test
+    public void it_extracts_insdc_dates(){
+        Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
+        Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("INSDC first public"))
+                .findFirst();
+
+        assertTrue(expectedAttribute.isPresent());
+
+        Attribute insdcFirstPublic = expectedAttribute.get();
+        assertEquals(insdcFirstPublic.getValue(), "2018-07-01T00:50:05.513Z");
+
+        expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("INSDC last update"))
+                .findFirst();
+
+        assertTrue(expectedAttribute.isPresent());
+
+        Attribute insdcLastUpdate = expectedAttribute.get();
+        assertEquals(insdcLastUpdate.getValue(), "2018-07-01T00:50:05.513Z");
+
+
+    }
+
+    @Test
+    public void it_extracts_insdc_status(){
+        Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(this.testNcbiBioSamples);
+        Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("INSDC status"))
+                .findFirst();
+
+        assertTrue(expectedAttribute.isPresent());
+
+        Attribute insdcStatus = expectedAttribute.get();
+        assertEquals(insdcStatus.getValue(), "live");
+
+    }
+
+    @Test
+    public void given_ncbi_status_not_live_it_set_release_date_in_the_future() throws DocumentException {
+            Element ncbiSampleNotLive = readBioSampleElementFromXml("/examples/ncbi_test_sample_not_live.xml");
+            Sample sampleToTest = this.conversionService.convertNcbiXmlElementToSample(ncbiSampleNotLive);
+            Optional<Attribute> expectedAttribute = sampleToTest.getAttributes().stream()
+                .filter( attr -> attr.getType().equals("INSDC status"))
+                .findFirst();
+
+            assertTrue(expectedAttribute.isPresent());
+            assertNotEquals(expectedAttribute.get().getValue(), "live");
+
+            // Sample release date is set in the future
+            assertTrue(sampleToTest.getRelease().isAfter(Instant.parse("3018-07-01T00:50:05.00Z")));
+
 
     }
 
