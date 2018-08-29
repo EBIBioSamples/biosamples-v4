3ddca10931076116ea7e24364cbba7dec242d20e
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-08-29 09:58:03 +0100

    Updated sampleId test for single taxId

--- models/core/src/test/java/uk/ac/ebi/biosamples/model/SampleTaxIdTest.java
+++ models/core/src/test/java/uk/ac/ebi/biosamples/model/SampleTaxIdTest.java
@@ -13,7 +13,6 @@ public class SampleTaxIdTest {
     @Test
     public void given_single_ontologyTerm_return_taxId() {
         String olsValue = "http://purl.obolibrary.org/obo/NCBITaxon_10116";
-        Integer[] taxIds = new Integer[]{10116};
         Attribute attribute = Attribute.build("Organism", "", Collections.singletonList(olsValue), null);
         Sample sample = generateTestSample(Collections.singletonList(attribute));
         assertTrue(10116 == sample.getTaxId());
@@ -22,7 +21,6 @@ public class SampleTaxIdTest {
     @Test
     public void given_single_ontologyTerm_return_taxId_with_lowercase_organism() {
         String olsValue = "http://purl.obolibrary.org/obo/NCBITaxon_9685";
-        Integer[] taxIds = new Integer[]{9685};
         Attribute attribute = Attribute.build("organism", "Felis catu", Collections.singletonList(olsValue), null);
         Sample sample = generateTestSample(Collections.singletonList(attribute));
         assertTrue(9685 == sample.getTaxId());
@@ -31,7 +29,6 @@ public class SampleTaxIdTest {
     @Test
     public void given_an_Organism_with_multiple_entries() {
         List<String> olsValues = Arrays.asList("http://purl.obolibrary.org/obo/NCBITaxon_10116", "http://purl.obolibrary.org/obo/NCBITaxon_9685");
-        Integer[] taxIds = new Integer[]{10116, 9685};
         Attribute attribute = Attribute.build("Organism", "Felis catu", olsValues, null);
         Sample sample = generateTestSample(Collections.singletonList(attribute));
         assertTrue(10116 == sample.getTaxId());
@@ -41,7 +38,6 @@ public class SampleTaxIdTest {
     public void given_multiple_Organisms() {
         String olsValue1 = "http://purl.obolibrary.org/obo/NCBITaxon_10116";
         String olsValue2 = "http://purl.obolibrary.org/obo/NCBITaxon_9685";
-        Integer[] taxIds = new Integer[]{10116, 9685};
         Attribute attribute1 = Attribute.build("Organism", "", olsValue1, null);
         Attribute attribute2 = Attribute.build("Organism", "", olsValue2, null);
         Sample sample = generateTestSample(Arrays.asList(attribute1, attribute2));
