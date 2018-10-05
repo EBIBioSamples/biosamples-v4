f041550409ffbc08c9a07c3b70f07740bfb46d2f
Luca Cherubin <luca.cherubin@gmail.com>
2018-10-05 17:04:56 +0200

    Makes post request possible if superUser

--- webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/SamplesRestController.java
+++ webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/SamplesRestController.java
@@ -36,9 +36,9 @@ import java.util.stream.Collectors;
 /**
  * Primary controller for REST operations both in JSON and XML and both read and
  * write.
- * 
+ *
  * See {@link SampleHtmlController} for the HTML equivalent controller.
- * 
+ *
  * @author faulcon
  *
  */
@@ -81,20 +81,20 @@ public class SamplesRestController {
 	@GetMapping(produces = { MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<Resources<Resource<Sample>>> searchHal(
 			@RequestParam(name = "text", required = false) String text,
-			@RequestParam(name = "filter", required = false) String[] filter, 
+			@RequestParam(name = "filter", required = false) String[] filter,
 			@RequestParam(name = "cursor", required = false) String cursor,
 			@RequestParam(name = "page", required = false) final Integer page,
-			@RequestParam(name = "size", required = false) final Integer size, 
-			@RequestParam(name = "sort", required = false) final String[] sort, 
+			@RequestParam(name = "size", required = false) final Integer size,
+			@RequestParam(name = "sort", required = false) final String[] sort,
 			PagedResourcesAssembler<Sample> pageAssembler) {
 
-		
+
 		//Need to decode the %20 and similar from the parameters
 		//this is *not* needed for the html controller
 		String decodedText = LinkUtils.decodeText(text);
 		String[] decodedFilter = LinkUtils.decodeTexts(filter);
 		String decodedCursor = LinkUtils.decodeText(cursor);
-			
+
 		int effectivePage;
 		if (page == null) {
 			effectivePage = 0;
@@ -107,10 +107,10 @@ public class SamplesRestController {
 		} else {
 			effectiveSize = size;
 		}
-		
+
 		Collection<Filter> filters = filterService.getFiltersCollection(decodedFilter);
 		Collection<String> domains = bioSamplesAapService.getDomains();
-		
+
 
 		//Note - EBI load balancer does cache but doesn't add age header, so clients could cache up to twice this age
 		CacheControl cacheControl = CacheControl.maxAge(bioSamplesProperties.getBiosamplesCorePageCacheMaxAge(), TimeUnit.SECONDS);
@@ -122,73 +122,73 @@ public class SamplesRestController {
 		if (cursor != null) {
 
 			log.trace("This cursor = "+decodedCursor);
-			CursorArrayList<Sample> samples = samplePageService.getSamplesByText(decodedText, filters, 
+			CursorArrayList<Sample> samples = samplePageService.getSamplesByText(decodedText, filters,
 				domains, decodedCursor, effectiveSize);
 			log.trace("Next cursor = "+samples.getNextCursorMark());
-			
+
 			Resources<Resource<Sample>>  resources = new Resources<>(samples.stream()
 					.map(s -> s != null ? sampleResourceAssembler.toResource(s) : null)
 				.collect(Collectors.toList()));
 
 			resources.add(getCursorLink(decodedText, decodedFilter, decodedCursor, effectiveSize, Link.REL_SELF));
 			//only display the next link if there is a next cursor to go to
-			if (!LinkUtils.decodeText(samples.getNextCursorMark()).equals(decodedCursor) 
+			if (!LinkUtils.decodeText(samples.getNextCursorMark()).equals(decodedCursor)
 					&& !samples.getNextCursorMark().equals("*")) {
-				resources.add(getCursorLink(decodedText, decodedFilter, samples.getNextCursorMark(), effectiveSize, Link.REL_NEXT));				
+				resources.add(getCursorLink(decodedText, decodedFilter, samples.getNextCursorMark(), effectiveSize, Link.REL_NEXT));
 			}
-			
+
 			//Note - EBI load balancer does cache but doesn't add age header, so clients could cache up to twice this age
 			return ResponseEntity.ok()
 					.cacheControl(cacheControl)
 					.body(resources);
-			
-		} else {	
-			
+
+		} else {
+
 			String effectiveSort[] = sort;
 			if (sort == null) {
 				//if there is no existing sort, sort by score then accession
 				effectiveSort = new String[2];
 				effectiveSort[0] = "score,desc";
 				effectiveSort[1] = "id,asc";
-			} 
+			}
 			Sort pageSort = new Sort(Arrays.stream(effectiveSort).map(this::parseSort).collect(Collectors.toList()));
 			Pageable pageable = new PageRequest(effectivePage, effectiveSize, pageSort);
-			
+
 			Page<Sample> pageSample = samplePageService.getSamplesByText(text, filters, domains, pageable);
 
-			
+
 			PageMetadata pageMetadata = new PageMetadata(effectiveSize,
 					pageSample.getNumber(), pageSample.getTotalElements(), pageSample.getTotalPages());
-			
+
 			Resources<Resource<Sample>> resources = new PagedResources<>(pageSample.getContent().stream()
 					.map(s -> s != null ? sampleResourceAssembler.toResource(s) : null)
-					.collect(Collectors.toList()), pageMetadata);			 
+					.collect(Collectors.toList()), pageMetadata);
 
 
 			//if theres more than one page, link to first and last
 			if (pageSample.getTotalPages() > 1) {
-				resources.add(getPageLink(decodedText, decodedFilter, 0, effectiveSize, sort, Link.REL_FIRST));				
+				resources.add(getPageLink(decodedText, decodedFilter, 0, effectiveSize, sort, Link.REL_FIRST));
 			}
 			//if there was a previous page, link to it
 			if (effectivePage > 0) {
 				resources.add(getPageLink(decodedText, decodedFilter, effectivePage-1, effectiveSize, sort, Link.REL_PREVIOUS));
 			}
 			resources.add(getPageLink(decodedText, decodedFilter, effectivePage, effectiveSize, sort, Link.REL_SELF));
-			
-			//if there is a next page, link to it 
+
+			//if there is a next page, link to it
 			if (effectivePage < pageSample.getTotalPages()-1) {
 				resources.add(getPageLink(decodedText, decodedFilter, effectivePage+1, effectiveSize, sort, Link.REL_NEXT));
 			}
 			//if theres more than one page, link to first and last
 			if (pageSample.getTotalPages() > 1) {
-				resources.add(getPageLink(decodedText, decodedFilter, pageSample.getTotalPages(), effectiveSize, sort, Link.REL_LAST));				
+				resources.add(getPageLink(decodedText, decodedFilter, pageSample.getTotalPages(), effectiveSize, sort, Link.REL_LAST));
 			}
 
 			//if we are on the first page and not sorting
 			if (effectivePage==0 && (sort==null || sort.length==0)) {
 				resources.add(getCursorLink(decodedText, decodedFilter, "*", effectiveSize, "cursor"));
 			}
-			
+
 			//if there is no search term, and on first page, add a link to use search
 			//TODO
 //			if (text.trim().length() == 0 && page == 0) {
@@ -197,20 +197,20 @@ public class SamplesRestController {
 //						.searchHal(null, filter, null, page, effectiveSize, sort, null))
 //					.withRel("search")));
 //			}
-			
+
 			resources.add(SampleAutocompleteRestController.getLink(decodedText, decodedFilter, null, "autocomplete"));
-			
-			
+
+
 			UriComponentsBuilder uriComponentsBuilder = ControllerLinkBuilder.linkTo(SamplesRestController.class).toUriComponentsBuilder();
 			//This is a bit of a hack, but best we can do for now...
 			resources.add(new Link(uriComponentsBuilder.build(true).toUriString()+"/{accession}", "sample"));
-			
+
 			return ResponseEntity.ok()
 					.cacheControl(cacheControl)
 					.body(resources);
 		}
 	}
-	
+
 	private Order parseSort(String sort) {
 		if(sort.endsWith(",desc")) {
 			return new Order(Sort.Direction.DESC, sort.substring(0, sort.length()-5));
@@ -220,11 +220,11 @@ public class SamplesRestController {
 			return new Order(null, sort);
 		}
 	}
-	
+
 	/**
 	 * ControllerLinkBuilder seems to have problems linking to the same controller?
 	 * Split out into manual manipulation for greater control
-	 * 
+	 *
 	 * @param text
 	 * @param filter
 	 * @param cursor
@@ -240,14 +240,14 @@ public class SamplesRestController {
 		}
 		if (filter != null) {
 			for (String filterString : filter) {
-				builder.queryParam("filter",filterString);				
+				builder.queryParam("filter",filterString);
 			}
 		}
 		builder.queryParam("cursor", cursor);
 		builder.queryParam("size", size);
 		return new Link(builder.toUriString(), rel);
 	}
-	
+
 	public static Link getPageLink(String text, String[] filter, int page, int size, String[] sort, String rel) {
 		UriComponentsBuilder builder = ControllerLinkBuilder.linkTo(SamplesRestController.class)
 				.toUriComponentsBuilder();
@@ -256,19 +256,19 @@ public class SamplesRestController {
 		}
 		if (filter != null) {
 			for (String filterString : filter) {
-				builder.queryParam("filter",filterString);				
+				builder.queryParam("filter",filterString);
 			}
 		}
 		builder.queryParam("page", page);
 		builder.queryParam("size", size);
 		if (sort != null) {
 			for (String sortString : sort) {
-				builder.queryParam("sort",sortString);				
+				builder.queryParam("sort",sortString);
 			}
 		}
 		return new Link(builder.toUriString(), rel);
 	}
-	
+
 
 
 	@PreAuthorize("isAuthenticated()")
@@ -276,14 +276,15 @@ public class SamplesRestController {
 	public ResponseEntity<Resource<Sample>> post(@RequestBody Sample sample,
 			@RequestParam(name = "setupdatedate", required = false, defaultValue="true") boolean setUpdateDate,
             @RequestParam(name = "setfulldetails", required = false, defaultValue = "false") boolean setFullDetails) {
-		
+
 		log.debug("Recieved POST for "+sample);
-		if (sample.hasAccession()) {
+		if (sample.hasAccession() && !bioSamplesAapService.isWriteSuperUser()) {
+			// Throw an error only if the user is not a super user and is trying to post a sample with an accession
 			throw new SampleWithAccessionSumbissionException();
 		}
 
 		sample = bioSamplesAapService.handleSampleDomain(sample);
-		
+
 		//TODO disallow previously accessioned samples - BSD-1186
 
 		//TODO disallow previously accessioned samples - BSD-1186
@@ -301,22 +302,22 @@ public class SamplesRestController {
 			sample = sampleManipulationService.removeLegacyFields(sample);
 		}
 
-		if (!sample.getData().isEmpty()) {
-			//TODO reanable the validation once the AMR schema is defined and we have the actual validator in place
-			Optional<ResponseEntity<String>> optionalInvalidResponse = sample.getData()
-					.parallelStream()
-					.map(abstractData -> schemaValidatorService.validate(abstractData.getStructuredData(), abstractData.getSchema()))
-                    .filter(response -> !response.getBody().equalsIgnoreCase("[]"))
-                    .findAny();
-			if (optionalInvalidResponse.isPresent()) {
-				throw new SampleWithInvalidStructuredData(optionalInvalidResponse.get().getBody());
-			}
+//			//TODO reanable the validation once the AMR schema is defined and we have the actual validator in place
+//		if (!sample.getData().isEmpty()) {
+//			Optional<ResponseEntity<String>> optionalInvalidResponse = sample.getData()
+//					.parallelStream()
+//					.map(abstractData -> schemaValidatorService.validate(abstractData.getStructuredData(), abstractData.getSchema()))
+//                    .filter(response -> !response.getBody().equalsIgnoreCase("[]"))
+//                    .findAny();
+//			if (optionalInvalidResponse.isPresent()) {
+//				throw new SampleWithInvalidStructuredData(optionalInvalidResponse.get().getBody());
+//			}
+//
+//		}
 
-		}
 
-		
 		sample = sampleService.store(sample);
-		
+
 		// assemble a resource to return
 		Resource<Sample> sampleResource = sampleResourceAssembler.toResource(sample);
 
