7984d4d37ff9d5662d0806d1484ea88952293226
Luca Cherubin <luca.cherubin@gmail.com>
2018-10-12 10:04:24 +0100

    Moves to 4.1.6-rc2 to update the release

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
   
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.6-rc1.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.6-rc2-SNAPSHOT.jar
 echo "Successfully runned agents"
--- docker-test.sh
+++ docker-test.sh
@@ -5,7 +5,7 @@ docker-compose up -d biosamples-agents-solr
 
 for X in "$@"
 do
-  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.1.6-rc1.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.1.6-rc2-SNAPSHOT.jar --phase=$X $ARGS $@
   sleep 30 #solr is configured to commit every 5 seconds
 
 done
