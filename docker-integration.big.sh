e795c83dcc3c434f8dcd2b54288f05da95927df5
Luca Cherubin <luca.cherubin@gmail.com>
2018-10-12 11:50:23 +0100

    Changes one test to make the big integration test not interfering with other integration tests

---
+++ docker-integration.big.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+./docker-webapp.sh --clean
+
+
+docker-compose up -d biosamples-agents-solr
+
+ARGS=--spring.profiles.active=big
+for X in 1 2 3 4 5
+do
+  #java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-4.1.6-rc2-SNAPSHOT.jar --phase=$X $ARGS $@
+  sleep 10 #solr is configured to commit every 5 seconds
+
+done
+
+#leave the agent up at the end
+docker-compose up -d biosamples-agents-solr
+
+echo "Successfully completed"
