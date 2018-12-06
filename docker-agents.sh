c6b1889ae7dd019b245841665bb4bfe363e7218e
Luca Cherubin <luca.cherubin@gmail.com>
2018-12-06 16:05:45 +0000

    Moves to 4.1.7-rc3

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.7-rc2.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.7-rc3.jar
 echo "Successfully runned agents"
