60562f5b3bc419aa24c45244ea6f7bbe67b7984d
Daniel Vaughan <dvaughan@ebi.ac.uk>
2019-02-07 13:48:22 +0000

    added handling of suppressed and temporary suppressed

--- pipelines/ena/src/main/java/uk/ac/ebi/biosamples/ena/EnaRunner.java
+++ pipelines/ena/src/main/java/uk/ac/ebi/biosamples/ena/EnaRunner.java
@@ -17,6 +17,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -27,154 +28,202 @@ import java.util.concurrent.Future;
 @ConditionalOnProperty(prefix = "job.autorun", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class EnaRunner implements ApplicationRunner {
 
-	private Logger log = LoggerFactory.getLogger(getClass());
+    private final static Logger log = LoggerFactory.getLogger(EnaRunner.class);
 
-	@Autowired
-	private PipelinesProperties pipelinesProperties;
+    @Autowired
+    private PipelinesProperties pipelinesProperties;
 
-	@Autowired
-	private EraProDao eraProDao;
-	
-	@Autowired
-	private EnaCallableFactory enaCallableFactory;
-	
-	@Autowired
-	private NcbiCurationCallableFactory ncbiCallableFactory;
+    @Autowired
+    private EraProDao eraProDao;
 
-	private Map<String, Future<Void>> futures = new LinkedHashMap<>();
-	
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
+    @Autowired
+    private EnaCallableFactory enaCallableFactory;
 
-		log.info("Processing ENA pipeline...");
+    @Autowired
+    private NcbiCurationCallableFactory ncbiCallableFactory;
 
-		// date format is YYYY-mm-dd
-		LocalDate fromDate = null;
-		if (args.getOptionNames().contains("from")) {
-			fromDate = LocalDate.parse(args.getOptionValues("from").iterator().next(),
-					DateTimeFormatter.ISO_LOCAL_DATE);
-		} else {
-			fromDate = LocalDate.parse("1000-01-01", DateTimeFormatter.ISO_LOCAL_DATE);
-		}
-		LocalDate toDate = null;
-		if (args.getOptionNames().contains("until")) {
-			toDate = LocalDate.parse(args.getOptionValues("until").iterator().next(), DateTimeFormatter.ISO_LOCAL_DATE);
-		} else {
-			toDate = LocalDate.parse("3000-01-01", DateTimeFormatter.ISO_LOCAL_DATE);
-		}
-		
-		if (pipelinesProperties.getThreadCount() == 0) {
-			EraRowCallbackHandler eraRowCallbackHandler = new EraRowCallbackHandler(null, enaCallableFactory, futures);
-			eraProDao.doSampleCallback(fromDate, toDate, eraRowCallbackHandler);
-			
-			NcbiRowCallbackHandler ncbiRowCallbackHandler = new NcbiRowCallbackHandler(null, ncbiCallableFactory, futures);
-			eraProDao.getNcbiCallback(fromDate, toDate, ncbiRowCallbackHandler);
-		} else {
-		
-			try (AdaptiveThreadPoolExecutor executorService = AdaptiveThreadPoolExecutor.create(100, 10000, false, 
-					pipelinesProperties.getThreadCount(), pipelinesProperties.getThreadCountMax())) {
-	
-				EraRowCallbackHandler eraRowCallbackHandler = new EraRowCallbackHandler(executorService, enaCallableFactory, futures);
-				eraProDao.doSampleCallback(fromDate, toDate, eraRowCallbackHandler);
-				
-				NcbiRowCallbackHandler ncbiRowCallbackHandler = new NcbiRowCallbackHandler(executorService, ncbiCallableFactory, futures);
-				eraProDao.getNcbiCallback(fromDate, toDate, ncbiRowCallbackHandler);
-				
-				log.info("waiting for futures");
-				// wait for anything to finish
-				ThreadUtils.checkFutures(futures, 0);
-			} 
-		}
-	}
-	
-	
-	private static class EraRowCallbackHandler implements RowCallbackHandler {
+    private Map<String, Future<Void>> futures = new LinkedHashMap<>();
 
-		private final AdaptiveThreadPoolExecutor executorService;
-		private final EnaCallableFactory enaCallableFactory;
-		private final Map<String, Future<Void>> futures;
-		
-		public EraRowCallbackHandler(AdaptiveThreadPoolExecutor executorService, 
-				EnaCallableFactory enaCallableFactory, 
-				Map<String, Future<Void>>futures) {
-			this.executorService = executorService;
-			this.enaCallableFactory = enaCallableFactory;
-			this.futures = futures;
-		}
-		
-		@Override
-		public void processRow(ResultSet rs) throws SQLException {
-			String sampleAccession = rs.getString("BIOSAMPLE_ID");
-			
-			Callable<Void> callable = enaCallableFactory.build(sampleAccession); 
-			if (executorService == null) {
-				try {
-					callable.call();
-				} catch (RuntimeException e) {
-					throw e;
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-			} else {
-				futures.put(sampleAccession, executorService.submit(callable));
-				try {
-					ThreadUtils.checkFutures(futures, 100);
-				} catch (RuntimeException e) {
-					throw e;
-				} catch (ExecutionException e) {
-					throw new RuntimeException(e.getCause());
-				}catch (InterruptedException e) {
-					throw new RuntimeException(e);
-				}
-			}
-		}
-		
-	}
-	
-	private static class NcbiRowCallbackHandler implements RowCallbackHandler {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
 
-		private final AdaptiveThreadPoolExecutor executorService;
-		private final NcbiCurationCallableFactory ncbiCallableFactory;
-		private final Map<String, Future<Void>> futures;
+        log.info("Processing ENA pipeline...");
 
-		private Logger log = LoggerFactory.getLogger(getClass());
-		
-		public NcbiRowCallbackHandler(AdaptiveThreadPoolExecutor executorService,
-				NcbiCurationCallableFactory ncbiCallableFactory,
-				Map<String, Future<Void>> futures) {
-			this.executorService = executorService;
-			this.ncbiCallableFactory = ncbiCallableFactory;
-			this.futures = futures;
-		}
-		
-		@Override
-		public void processRow(ResultSet rs) throws SQLException {
-			String sampleAccession = rs.getString("BIOSAMPLE_ID");
-			
-			Callable<Void> callable = ncbiCallableFactory.build(sampleAccession); 
-			if (executorService == null) {
-				try {
-					callable.call();
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-			} else {
-				futures.put(sampleAccession, executorService.submit(callable));
-				try {
-					ThreadUtils.checkFutures(futures, 100);
-				} catch (HttpClientErrorException e) {
-					log.error("HTTP Client error body : "+e.getResponseBodyAsString());
-					throw e;
-				} catch (RuntimeException e) {
-					throw e;
-				} catch (ExecutionException e) {
-					throw new RuntimeException(e.getCause());
-				}catch (InterruptedException e) {
-					throw new RuntimeException(e);
-				}
-			}
-		}
-		
-	}
-	
+        // date format is YYYY-mm-dd
+        LocalDate fromDate = null;
+        if (args.getOptionNames().contains("from")) {
+            fromDate = LocalDate.parse(args.getOptionValues("from").iterator().next(),
+                    DateTimeFormatter.ISO_LOCAL_DATE);
+        } else {
+            fromDate = LocalDate.parse("1000-01-01", DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+        LocalDate toDate = null;
+        if (args.getOptionNames().contains("until")) {
+            toDate = LocalDate.parse(args.getOptionValues("until").iterator().next(), DateTimeFormatter.ISO_LOCAL_DATE);
+        } else {
+            toDate = LocalDate.parse("3000-01-01", DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+
+        if (pipelinesProperties.getThreadCount() == 0) {
+            EraRowCallbackHandler eraRowCallbackHandler = new EraRowCallbackHandler(null, enaCallableFactory, futures);
+            eraProDao.doSampleCallback(fromDate, toDate, eraRowCallbackHandler);
+
+            /*
+            NcbiRowCallbackHandler ncbiRowCallbackHandler = new NcbiRowCallbackHandler(null, ncbiCallableFactory, futures);
+            eraProDao.getNcbiCallback(fromDate, toDate, ncbiRowCallbackHandler);
+            */
+        } else {
+
+            try (AdaptiveThreadPoolExecutor executorService = AdaptiveThreadPoolExecutor.create(100, 10000, false,
+                    pipelinesProperties.getThreadCount(), pipelinesProperties.getThreadCountMax())) {
+
+                EraRowCallbackHandler eraRowCallbackHandler = new EraRowCallbackHandler(executorService, enaCallableFactory, futures);
+                eraProDao.doSampleCallback(fromDate, toDate, eraRowCallbackHandler);
+
+                NcbiRowCallbackHandler ncbiRowCallbackHandler = new NcbiRowCallbackHandler(executorService, ncbiCallableFactory, futures);
+                eraProDao.getNcbiCallback(fromDate, toDate, ncbiRowCallbackHandler);
+
+                log.info("waiting for futures");
+                // wait for anything to finish
+                ThreadUtils.checkFutures(futures, 0);
+            }
+        }
+    }
+
+    private static class EraRowCallbackHandler implements RowCallbackHandler {
+
+        private final AdaptiveThreadPoolExecutor executorService;
+        private final EnaCallableFactory enaCallableFactory;
+        private final Map<String, Future<Void>> futures;
+
+        public EraRowCallbackHandler(AdaptiveThreadPoolExecutor executorService,
+                                     EnaCallableFactory enaCallableFactory,
+                                     Map<String, Future<Void>> futures) {
+            this.executorService = executorService;
+            this.enaCallableFactory = enaCallableFactory;
+            this.futures = futures;
+        }
+
+        private enum ENAStatus {
+            PUBLIC(4),
+            SUPPRESSED(5),
+            KILLED(6),
+            TEMPORARY_SUPPRESSED(7),
+            TEMPORARY_KILLED(8);
+
+            private int value;
+            private static Map map = new HashMap<>();
+
+            ENAStatus(int value) {
+                this.value = value;
+            }
+
+            static {
+                for (ENAStatus enaStatus : ENAStatus.values()) {
+                    map.put(enaStatus.value, enaStatus);
+                }
+            }
+
+            public static ENAStatus valueOf(int pageType) {
+                return (ENAStatus) map.get(pageType);
+            }
+
+            public int getValue() {
+                return value;
+            }
+        }
+
+        @Override
+        public void processRow(ResultSet rs) throws SQLException {
+            String sampleAccession = rs.getString("BIOSAMPLE_ID");
+            int statusID = rs.getInt("STATUS_ID");
+            ENAStatus enaStatus = ENAStatus.valueOf(statusID);
+            switch (enaStatus) {
+                case PUBLIC:
+                case SUPPRESSED:
+                case TEMPORARY_SUPPRESSED:
+                    log.info(String.format("%s is being handled as status is %s", sampleAccession, enaStatus.name()));
+                    //update if sample already exists else import
+                    Callable<Void> callable = enaCallableFactory.build(sampleAccession);
+                    if (executorService == null) {
+                        try {
+                            callable.call();
+                        } catch (RuntimeException e) {
+                            throw e;
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        futures.put(sampleAccession, executorService.submit(callable));
+                        try {
+                            ThreadUtils.checkFutures(futures, 100);
+                        } catch (RuntimeException e) {
+                            throw e;
+                        } catch (ExecutionException e) {
+                            throw new RuntimeException(e.getCause());
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    break;
+                case KILLED:
+                case TEMPORARY_KILLED:
+                    //TODO
+                    //set hold date
+                    //update if sample already exists else ignore
+                    log.info(String.format("%s would be handled as status is %s", sampleAccession, enaStatus.name()));
+                    break;
+                default:
+                    log.info(String.format("%s would be ignored  as status is %s", sampleAccession, enaStatus.name()));
+            }
+        }
+
+    }
+
+    private static class NcbiRowCallbackHandler implements RowCallbackHandler {
+
+        private final AdaptiveThreadPoolExecutor executorService;
+        private final NcbiCurationCallableFactory ncbiCallableFactory;
+        private final Map<String, Future<Void>> futures;
+
+        private Logger log = LoggerFactory.getLogger(getClass());
+
+        public NcbiRowCallbackHandler(AdaptiveThreadPoolExecutor executorService,
+                                      NcbiCurationCallableFactory ncbiCallableFactory,
+                                      Map<String, Future<Void>> futures) {
+            this.executorService = executorService;
+            this.ncbiCallableFactory = ncbiCallableFactory;
+            this.futures = futures;
+        }
+
+        @Override
+        public void processRow(ResultSet rs) throws SQLException {
+            String sampleAccession = rs.getString("BIOSAMPLE_ID");
+
+            Callable<Void> callable = ncbiCallableFactory.build(sampleAccession);
+            if (executorService == null) {
+                try {
+                    callable.call();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                futures.put(sampleAccession, executorService.submit(callable));
+                try {
+                    ThreadUtils.checkFutures(futures, 100);
+                } catch (HttpClientErrorException e) {
+                    log.error("HTTP Client error body : " + e.getResponseBodyAsString());
+                    throw e;
+                } catch (RuntimeException e) {
+                    throw e;
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e.getCause());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+
 }
--- pipelines/ena/src/main/java/uk/ac/ebi/biosamples/ena/EraProDao.java
+++ pipelines/ena/src/main/java/uk/ac/ebi/biosamples/ena/EraProDao.java
@@ -19,11 +19,14 @@ import java.util.TreeSet;
 @Service
 public class EraProDao {
 
+
     @Autowired
     @Qualifier("eraJdbcTemplate")
     protected JdbcTemplate jdbcTemplate;
 
     private Logger log = LoggerFactory.getLogger(getClass());
+
+    private static final String STATUS_CLAUS = "STATUS_ID IN (4, 5, 6, 7, 8)";
 
     /**
      * Return a set of BioSamples accessions that have been updated or made public within
@@ -48,7 +51,7 @@ select * from cv_status;
         //once it has been public, it can only be suppressed and killed and can't go back to public again
 
         String query = "SELECT BIOSAMPLE_ID FROM SAMPLE WHERE BIOSAMPLE_ID LIKE 'SAME%' AND EGA_ID IS NULL AND BIOSAMPLE_AUTHORITY= 'N' "
-                + "AND STATUS_ID = 4 AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?)) ORDER BY BIOSAMPLE_ID ASC";
+                + "AND " + STATUS_CLAUS + " AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?)) ORDER BY BIOSAMPLE_ID ASC";
 
         SortedSet<String> samples = new TreeSet<>();
         Date minDateOld = java.sql.Date.valueOf(minDate);
@@ -60,8 +63,8 @@ select * from cv_status;
 
 
     public void doSampleCallback(LocalDate minDate, LocalDate maxDate, RowCallbackHandler rch) {
-        String query = "SELECT UNIQUE(BIOSAMPLE_ID) FROM SAMPLE WHERE BIOSAMPLE_ID LIKE 'SAME%' AND SAMPLE_ID LIKE 'ERS%' AND EGA_ID IS NULL AND BIOSAMPLE_AUTHORITY= 'N' "
-                + "AND STATUS_ID = 4 AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?)) ORDER BY BIOSAMPLE_ID ASC";
+        String query = "SELECT UNIQUE(BIOSAMPLE_ID), STATUS_ID FROM SAMPLE WHERE BIOSAMPLE_ID LIKE 'SAME%' AND SAMPLE_ID LIKE 'ERS%' AND EGA_ID IS NULL AND BIOSAMPLE_AUTHORITY= 'N' "
+                + "AND " + STATUS_CLAUS + " AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?)) ORDER BY BIOSAMPLE_ID ASC";
 
         Date minDateOld = java.sql.Date.valueOf(minDate);
         Date maxDateOld = java.sql.Date.valueOf(maxDate);
@@ -69,15 +72,15 @@ select * from cv_status;
     }
 
     public void getSingleSample(String bioSampleId, RowCallbackHandler rch) {
-        String query = "SELECT UNIQUE(BIOSAMPLE_ID) FROM SAMPLE WHERE BIOSAMPLE_ID LIKE 'SAME%' AND SAMPLE_ID LIKE 'ERS%' AND EGA_ID IS NULL AND BIOSAMPLE_AUTHORITY= 'N' "
-                + "AND STATUS_ID = 4 AND BIOSAMPLE_ID=? ORDER BY BIOSAMPLE_ID ASC";
+        String query = "SELECT UNIQUE(BIOSAMPLE_ID), STATUS_ID FROM SAMPLE WHERE BIOSAMPLE_ID LIKE 'SAME%' AND SAMPLE_ID LIKE 'ERS%' AND EGA_ID IS NULL AND BIOSAMPLE_AUTHORITY= 'N' "
+                + "AND " + STATUS_CLAUS + " AND BIOSAMPLE_ID=? ORDER BY BIOSAMPLE_ID ASC";
         jdbcTemplate.query(query, rch, bioSampleId);
     }
 
     public void getNcbiCallback(LocalDate minDate, LocalDate maxDate, RowCallbackHandler rch) {
 
-        String query = "SELECT UNIQUE(BIOSAMPLE_ID) FROM SAMPLE WHERE (BIOSAMPLE_ID LIKE 'SAMN%' OR BIOSAMPLE_ID LIKE 'SAMD%' ) "
-                + "AND STATUS_ID = 4 AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?)) ORDER BY BIOSAMPLE_ID ASC";
+        String query = "SELECT UNIQUE(BIOSAMPLE_ID), STATUS_ID FROM SAMPLE WHERE (BIOSAMPLE_ID LIKE 'SAMN%' OR BIOSAMPLE_ID LIKE 'SAMD%' ) "
+                + "AND " + STATUS_CLAUS + " AND ((LAST_UPDATED BETWEEN ? AND ?) OR (FIRST_PUBLIC BETWEEN ? AND ?)) ORDER BY BIOSAMPLE_ID ASC";
 
         Date minDateOld = java.sql.Date.valueOf(minDate);
         Date maxDateOld = java.sql.Date.valueOf(maxDate);
