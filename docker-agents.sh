63a8b184dcb7b79a847401a2c3674195e19a22a6
Daniel Vaughan <dvaughan@ebi.ac.uk>
2019-03-01 14:47:16 +0000

    Forth release candidate for BioSamples 4.1.12

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.2-RC3.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.12-RC4.jar
 echo "Successfully runned agents"
