0d303eb1fd5fb4208723beccdf6518a9061107e0
Daniel Vaughan <dvaughan@ebi.ac.uk>
2019-03-08 13:27:49 +0000

    added sample test pipeline

--- pipelines/sampletest/src/main/java/uk/ac/ebi/biosamples/sampletest/SampleTestApplicationRunner.java
+++ pipelines/sampletest/src/main/java/uk/ac/ebi/biosamples/sampletest/SampleTestApplicationRunner.java
@@ -9,10 +9,14 @@ import org.springframework.hateoas.Resource;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.biosamples.client.BioSamplesClient;
 import uk.ac.ebi.biosamples.model.Sample;
+import uk.ac.ebi.biosamples.service.FilterBuilder;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
 
 @Component
 public class SampleTestApplicationRunner implements ApplicationRunner {
@@ -28,6 +32,10 @@ public class SampleTestApplicationRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
+        getAllSamples();
+    }
+
+    private void getAllSamples() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("not-founds.txt"))) {
             long count = 0;
             long startTime = System.currentTimeMillis();
@@ -54,5 +62,29 @@ public class SampleTestApplicationRunner implements ApplicationRunner {
         } catch (Exception e) {
             log.error("Error getting samples resources", e);
         }
+    }
+
+    private Set<String> getExistingPublicNcbiAccessions() {
+        log.info("getting existing public ncbi accessions");
+        long startTime = System.nanoTime();
+        //make sure to only get the public samples
+        Set<String> existingAccessions = new TreeSet<>();
+        Iterable<Resource<Sample>> samples = bioSamplesClient.getPublicClient().get().fetchSampleResourceAll(Collections.singleton(FilterBuilder.create().onAccession("SAM[^E].*").build()));
+        for (Resource<Sample> sample : samples) {
+            existingAccessions.add(sample.getContent().getAccession());
+        }
+        long endTime = System.nanoTime();
+        double intervalSec = ((double) (endTime - startTime)) / 1000000000.0;
+        log.debug("Took " + intervalSec + "s to get " + existingAccessions.size() + " existing public ncbi accessions");
+        return existingAccessions;
+    }
+
+    private void getSAMD00000001() {
+        log.info("get SAMD00000001");
+        long startTime = System.nanoTime();
+        Optional<Resource<Sample>> sample = bioSamplesClient.getPublicClient().get().fetchSampleResource("SAMD00000001");
+        long endTime = System.nanoTime();
+        double intervalSec = ((double) (endTime - startTime)) / 1000000000.0;
+        log.info("Took " + intervalSec + "s to get SAMD00000001");
     }
 }
