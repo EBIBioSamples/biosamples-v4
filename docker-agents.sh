bf69674f3a48565f6ce84f4e7ab8d44c8bea95c1
Daniel Vaughan <dvaughan@ebi.ac.uk>
2019-03-27 11:00:51 +0000

    Defaulted null release dates to current dates

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.13.2.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.1.14-SNAPSHOT.jar
 echo "Successfully runned agents"
