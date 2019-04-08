86afd9e0f0fb4eb4881fc064d07ce9d1dbb1972f
Isuru Liyanage <theisuru@gmail.com>
2019-04-08 16:24:29 +0100

    add data folder check and include DUO codes

--- scripts/ega-import/src/main/java/uk/ac/ebi/biosamples/EGAImportRunner.java
+++ scripts/ega-import/src/main/java/uk/ac/ebi/biosamples/EGAImportRunner.java
@@ -14,7 +14,10 @@ import uk.ac.ebi.biosamples.model.Attribute;
 import uk.ac.ebi.biosamples.model.ExternalReference;
 import uk.ac.ebi.biosamples.model.Sample;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
 import java.util.*;
 
 @Component
@@ -30,23 +33,20 @@ public class EGAImportRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-//        String accession = "SAMEA5648975";
-//        String jwt = "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL2V4cGxvcmUuYWFpLmViaS5hYy51ay9zcCIsImp0aSI6IkUxSFE3SDFrME1ldjR3SGFnSUtvT0EiLCJpYXQiOjE1NTM2MTc3NDksInN1YiI6InVzci04NjYyYmEwNS0xMzRhLTRiMTQtODViMC04ZTUyY2I5ZmVlOGQiLCJlbWFpbCI6ImlzdXJ1QGViaS5hYy51ayIsIm5pY2tuYW1lIjoiaXN1cnVsIiwibmFtZSI6IklzdXJ1IExpeWFuYWdlIiwiZG9tYWlucyI6WyJzZWxmLklzdXJ1MSIsInN1YnMudGVzdC10ZWFtLTIzIiwic3Vicy5kZXYtdGVhbS0xNDAiXSwiZXhwIjoxNTUzNjIxMzQ5fQ.gOkxhArFcACn77_qieX0VA9gNsVyYjqK0VRN3ygGOEwN6_roPyajyPuLmePqC14FgzXcQDEwyYNmKN0LG8oUB7xncNifqblBmYQ29rneK3_e5nUnZEPKoHNkJeLo4VORS1TUVUhCm3VrILrbbX-wL5W3gh3vwH9nCUieL1LSNHjebeXomEz0unLl573iEdrVi-F7SpVkRFYiPZ0kCaoyEJ1YsYGP9pp18GmOfmWtxBXCPaEh9zvpY1_HL-F7TOV9Fb93EYcoVpRuZYpZH4pV-CRTNmOc6mz-yjhc1EOP9KmlS6sIryJhVG10IS-nnZWjj3-MdQBmlNseo1wVfWAdJw";
-//        Optional<Resource<Sample>> sampleResource = bioSamplesClient.fetchSampleResource(accession, jwt);
-//        if (sampleResource.isPresent()) {
-//            System.out.println(sampleResource.get().getContent());
-//        } else {
-//            System.out.println("Cant find the sample");
-//        }
+        if (args.getSourceArgs().length < 1) {
+            LOG.error("Please specify a data folder as a program argument");
+            throw new IllegalArgumentException("Please specify a data folder as a program argument");
+        }
 
-
-        final String dataFolderUrl = "/home/isuru/BioSamples/EGA_Import/";
+//        final String dataFolderUrl = "/home/isuru/BioSamples/EGA_Import/";
+        final String dataFolderUrl = args.getSourceArgs()[0];
         final String datasetDuoUrl = dataFolderUrl + "datasets_duo.csv";
         final String sampleDataUrl = dataFolderUrl + "sanger_released_samples.csv";
         final String egaUrl = "https://www.ebi.ac.uk/ega/datasets/";
         final ObjectMapper jsonMapper = new ObjectMapper();
 
-        Map<String, List<String>> datasetToDuoCodesMap = new HashMap<>();
+
+        Map<String, SortedSet<String>> datasetToDuoCodesMap = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(datasetDuoUrl))) {
             String line = br.readLine(); //ignore header
             LOG.info("Reading file: {}, headers: {}", datasetDuoUrl, line);
@@ -55,13 +55,12 @@ public class EGAImportRunner implements ApplicationRunner {
                 String datasetId = record[0];
                 String[] duoCodes = Arrays.copyOfRange(record, 1, record.length);
 
-                datasetToDuoCodesMap.put(datasetId, Arrays.asList(duoCodes));
+                datasetToDuoCodesMap.put(datasetId, new TreeSet<>(Arrays.asList(duoCodes)));
             }
 
         } catch (IOException e) {
             LOG.error("couldn't read file: " + datasetDuoUrl, e);
         }
-
 
         try (BufferedReader br = new BufferedReader(new FileReader(sampleDataUrl))) {
             String line = br.readLine(); //ignore header
@@ -72,7 +71,7 @@ public class EGAImportRunner implements ApplicationRunner {
                 String egaId = sampleValues[1];
                 String datasetId = sampleValues[2];
                 String phenotype = sampleValues[3];
-                List<String> duoCodes = datasetToDuoCodesMap.get(datasetId);
+                SortedSet<String> duoCodes = datasetToDuoCodesMap.get(datasetId);
 
                 Optional<Resource<Sample>> sampleResource = bioSamplesClient.fetchSampleResource(accession);
                 if (sampleResource.isPresent()) {
@@ -82,8 +81,8 @@ public class EGAImportRunner implements ApplicationRunner {
                             .addAttribute(Attribute.build("phenotype", phenotype))
                             .addAttribute(Attribute.build("ega dataset id", datasetId))
                             .addAttribute(Attribute.build("ega sample id", egaId))
-                            .addExternalReference(ExternalReference.build(egaUrl + datasetId))
-//                            .addAllDuoCodes(duoCodes)
+                            .addExternalReference(ExternalReference.build(egaUrl + datasetId, duoCodes))
+                            .withRelease(Instant.now())
                             .build();
 
                     LOG.info("Original sample: {}", jsonMapper.writeValueAsString(sample));
