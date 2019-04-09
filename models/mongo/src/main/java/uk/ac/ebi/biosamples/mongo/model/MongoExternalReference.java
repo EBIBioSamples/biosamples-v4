0f868a7c6c87b06383b0ba67d21a96626c7511cc
Isuru Liyanage <theisuru@gmail.com>
2019-04-09 17:21:17 +0100

    handle null pointer in DUO code

--- models/mongo/src/main/java/uk/ac/ebi/biosamples/mongo/model/MongoExternalReference.java
+++ models/mongo/src/main/java/uk/ac/ebi/biosamples/mongo/model/MongoExternalReference.java
@@ -1,44 +1,44 @@
 package uk.ac.ebi.biosamples.mongo.model;
 
-import java.nio.charset.Charset;
-import java.util.*;
-
-import com.google.common.hash.Hasher;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.hash.Hasher;
 import com.google.common.hash.Hashing;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class MongoExternalReference implements Comparable<MongoExternalReference> {
-	
-	private final String url;	
-	private final String hash;
-	private final SortedSet<String> duo;
 
-	private MongoExternalReference(String url, String hash, SortedSet<String> duo) {
-		this.url = url;
-		this.hash = hash;
-		this.duo = duo;
-	}
+    private final String url;
+    private final String hash;
+    private final SortedSet<String> duo;
 
-	public String getUrl() {
-		return this.url;
-	}
-	@JsonIgnore
-	public String getHash() {
-		return this.hash;
-	}
+    private MongoExternalReference(String url, String hash, SortedSet<String> duo) {
+        this.url = url;
+        this.hash = hash;
+        this.duo = duo;
+    }
 
-	public SortedSet<String> getDuo() {
-		return duo;
-	}
-	
-	@Override
+    public String getUrl() {
+        return this.url;
+    }
+
+    @JsonIgnore
+    public String getHash() {
+        return this.hash;
+    }
+
+    public SortedSet<String> getDuo() {
+        return duo;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (o == this) return true;
         if (!(o instanceof MongoExternalReference)) {
@@ -47,75 +47,86 @@ public class MongoExternalReference implements Comparable<MongoExternalReference
         MongoExternalReference other = (MongoExternalReference) o;
         return Objects.equals(this.url, other.url) && Objects.equals(this.duo, other.duo);
     }
-    
+
     @Override
     public int hashCode() {
-    	return Objects.hash(hash);
+        return Objects.hash(hash);
     }
 
-	@Override
-	public int compareTo(MongoExternalReference other) {
-		if (other == null) {
-			return 1;
-		}
-		
-		if (!this.url.equals(other.url)) {
-			return this.url.compareTo(other.url);
-		}
-		if (!this.duo.equals(other.duo)) {
-			if (this.duo.size() < other.duo.size()) {
-				return -1;
-			} else if (this.duo.size() > other.duo.size()) {
-				return 1;
-			} else {
-				Iterator<String> thisIt = this.duo.iterator();
-				Iterator<String> otherIt = other.duo.iterator();
-				while (thisIt.hasNext() && otherIt.hasNext()) {
-					int val = thisIt.next().compareTo(otherIt.next());
-					if (val != 0) return val;
-				}
-			}
-		}
-		return 0;
-	}	
+    @Override
+    public int compareTo(MongoExternalReference other) {
+        if (other == null) {
+            return 1;
+        }
+
+        if (!this.url.equals(other.url)) {
+            return this.url.compareTo(other.url);
+        }
+
+        if (this.duo == other.duo) {
+            return 0;
+        } else if (other.duo == null) {
+            return 1;
+        } else if (this.duo == null) {
+            return -1;
+        }
+
+        if (!this.duo.equals(other.duo)) {
+            if (this.duo.size() < other.duo.size()) {
+                return -1;
+            } else if (this.duo.size() > other.duo.size()) {
+                return 1;
+            } else {
+                Iterator<String> thisIt = this.duo.iterator();
+                Iterator<String> otherIt = other.duo.iterator();
+                while (thisIt.hasNext() && otherIt.hasNext()) {
+                    int val = thisIt.next().compareTo(otherIt.next());
+                    if (val != 0) return val;
+                }
+            }
+        }
+        return 0;
+    }
 
     @Override
     public String toString() {
-    	StringBuilder sb = new StringBuilder();
-    	sb.append("ExternalReference(");
-    	sb.append(this.url);
-		sb.append(",");
-		sb.append(duo);
-    	sb.append(")");
-    	return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("ExternalReference(");
+        sb.append(this.url);
+        sb.append(",");
+        sb.append(duo);
+        sb.append(")");
+        return sb.toString();
     }
 
     public static MongoExternalReference build(String url, SortedSet<String> duo) {
-		UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(url);
-		UriComponents uriComponents = uriComponentsBuilder.build().normalize();
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(url);
+        UriComponents uriComponents = uriComponentsBuilder.build().normalize();
 
-		url = uriComponents.toUriString();
+        url = uriComponents.toUriString();
 
-		Hasher hasher = Hashing.sha256().newHasher()
-				.putUnencodedChars(Objects.nonNull(uriComponents.getScheme()) ? uriComponents.getScheme() : "")
-				.putUnencodedChars(Objects.nonNull(uriComponents.getSchemeSpecificPart()) ? uriComponents.getSchemeSpecificPart() : "")
-				.putUnencodedChars(Objects.nonNull(uriComponents.getUserInfo()) ? uriComponents.getUserInfo() : "")
-				.putUnencodedChars(Objects.nonNull(uriComponents.getHost()) ? uriComponents.getHost() : "")
-				.putInt(Objects.nonNull(uriComponents.getPort()) ? uriComponents.getPort() : 0)
-				.putUnencodedChars(Objects.nonNull(uriComponents.getPath()) ? uriComponents.getPath() : "")
-				.putUnencodedChars(Objects.nonNull(uriComponents.getQuery()) ? uriComponents.getQuery() : "")
-				.putUnencodedChars(Objects.nonNull(uriComponents.getFragment()) ? uriComponents.getFragment() : "");
+        Hasher hasher = Hashing.sha256().newHasher()
+                .putUnencodedChars(Objects.nonNull(uriComponents.getScheme()) ? uriComponents.getScheme() : "")
+                .putUnencodedChars(Objects.nonNull(uriComponents.getSchemeSpecificPart()) ? uriComponents.getSchemeSpecificPart() : "")
+                .putUnencodedChars(Objects.nonNull(uriComponents.getUserInfo()) ? uriComponents.getUserInfo() : "")
+                .putUnencodedChars(Objects.nonNull(uriComponents.getHost()) ? uriComponents.getHost() : "")
+                .putInt(Objects.nonNull(uriComponents.getPort()) ? uriComponents.getPort() : 0)
+                .putUnencodedChars(Objects.nonNull(uriComponents.getPath()) ? uriComponents.getPath() : "")
+                .putUnencodedChars(Objects.nonNull(uriComponents.getQuery()) ? uriComponents.getQuery() : "")
+                .putUnencodedChars(Objects.nonNull(uriComponents.getFragment()) ? uriComponents.getFragment() : "");
 
-		for (String s : duo) {
-			hasher.putUnencodedChars(s);
-		}
+        if (duo != null) {
+            for (String s : duo) {
+                hasher.putUnencodedChars(s);
+            }
+        }
 
-		return new MongoExternalReference(url, hasher.hash().toString(), duo);
-	}
+        return new MongoExternalReference(url, hasher.hash().toString(), duo);
+    }
 
 
     @JsonCreator
-    public static MongoExternalReference build(@JsonProperty("url") String url) {    	
-    	return build(url, new TreeSet<>());
-	}
+    public static MongoExternalReference build(@JsonProperty("url") String url) {
+        return build(url, new TreeSet<>());
+    }
 }
