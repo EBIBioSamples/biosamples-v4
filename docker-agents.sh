dbb88d5b78183c6367700b7409810ae74942659d
dipayan1985 <dipayan.gupta0@gmail.com>
2019-10-30 14:52:16 +0000

    Move to next version of the software v4.2.2-SNAPSHOT

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.1-SNAPSHOT.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.2-SNAPSHOT.jar
 echo "Successfully runned agents"
