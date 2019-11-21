72f058638025bb04ff947fadbb5da649657b0a4f
dgupta <dgupta@ebi.ac.uk>
2019-11-21 14:21:22 +0000

    Improvements in copy down pipeline

--- pipelines/copydown/src/main/java/uk/ac/ebi/biosamples/copydown/SampleCopydownCallable.java
+++ pipelines/copydown/src/main/java/uk/ac/ebi/biosamples/copydown/SampleCopydownCallable.java
@@ -2,9 +2,10 @@ package uk.ac.ebi.biosamples.copydown;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.hateoas.Resource;
 import uk.ac.ebi.biosamples.client.BioSamplesClient;
-import uk.ac.ebi.biosamples.model.*;
+import uk.ac.ebi.biosamples.model.Attribute;
+import uk.ac.ebi.biosamples.model.Curation;
+import uk.ac.ebi.biosamples.model.Sample;
 
 import java.util.Collections;
 import java.util.HashSet;
@@ -14,12 +15,12 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class SampleCopydownCallable implements Callable<Void> {
-	private static final Attribute mixedAttribute = Attribute.build("organism", "mixed sample", "http://purl.obolibrary.org/obo/NCBITaxon_1427524", null);
+    private static final Attribute mixedAttribute = Attribute.build("organism", "mixed sample", "http://purl.obolibrary.org/obo/NCBITaxon_1427524", null);
     private final Sample sample;
     private final BioSamplesClient bioSamplesClient;
     private final String domain;
-	private Logger log = LoggerFactory.getLogger(getClass());
-	static final ConcurrentLinkedQueue<String> failedQueue = new ConcurrentLinkedQueue<>();
+    private Logger log = LoggerFactory.getLogger(getClass());
+    static final ConcurrentLinkedQueue<String> failedQueue = new ConcurrentLinkedQueue<>();
 
     public SampleCopydownCallable(final BioSamplesClient bioSamplesClient, final Sample sample, final String domain) {
         this.bioSamplesClient = bioSamplesClient;
@@ -29,10 +30,10 @@ public class SampleCopydownCallable implements Callable<Void> {
 
     @Override
     public Void call() {
-		final String accession = sample.getAccession();
+        final String accession = sample.getAccession();
         final boolean hasOrganism = sample.getAttributes().stream().anyMatch(attribute -> "organism".equals(attribute.getType().toLowerCase()));
         final boolean hasDerivedFrom = sample.getRelationships().stream().anyMatch
-				(relationship -> "derived from".equals(relationship.getType().toLowerCase()) && accession.equals(relationship.getSource()));
+                (relationship -> "derived from".equals(relationship.getType().toLowerCase()) && accession.equals(relationship.getSource()));
 
         if (!hasOrganism && hasDerivedFrom) {
             //walk up the derived from relationships and pull out all the organisms
@@ -52,7 +53,7 @@ public class SampleCopydownCallable implements Callable<Void> {
             }
         } else if (hasOrganism && hasDerivedFrom) {
             //this sample has an organism, but that might have been applied by a previous curation
-            for (final Resource<CurationLink> curationLink : bioSamplesClient.fetchCurationLinksOfSample(accession)) {
+            bioSamplesClient.fetchCurationLinksOfSample(accession).forEach(curationLink -> {
                 if (domain.equals(curationLink.getContent().getDomain())) {
                     SortedSet<Attribute> attributesPre = curationLink.getContent().getCuration().getAttributesPre();
                     SortedSet<Attribute> attributesPost = curationLink.getContent().getCuration().getAttributesPost();
@@ -88,7 +89,7 @@ public class SampleCopydownCallable implements Callable<Void> {
                         }
                     }
                 }
-            }
+            });
         }
 
         return null;
@@ -107,25 +108,21 @@ public class SampleCopydownCallable implements Callable<Void> {
         final Set<Attribute> organisms = new HashSet<>();
 
         if (!ignoreSample) {
-            for (Attribute attribute : sample.getAttributes()) {
-                if ("organism".equals(attribute.getType().toLowerCase())) {
-                    organisms.add(attribute);
-                }
-            }
+            sample.getAttributes().stream().filter(attribute -> "organism".equals(attribute.getType().toLowerCase())).forEach(attribute -> organisms.add(attribute));
         }
         //if there are no organisms directly, check derived from relationships
         if (organisms.size() == 0) {
             log.trace("" + sample.getAccession() + " has no organism");
 
-            for (final Relationship relationship : sample.getRelationships()) {
-                if ("derived from".equals(relationship.getType().toLowerCase()) && sample.getAccession().equals(relationship.getSource())) {
-                    log.trace("checking derived from " + relationship.getTarget());
+            sample.getRelationships().stream().
+                    filter(relationship -> "derived from".equals(relationship.getType().toLowerCase()) && sample.getAccession().equals(relationship.getSource())).
+                    forEach(relationship -> {
+                        log.trace("checking derived from " + relationship.getTarget());
 
-                    //recursion ahoy!
-                    bioSamplesClient.fetchSampleResource(relationship.getTarget())
-                            .ifPresent(sampleResource -> organisms.addAll(getOrganismsForSample(sampleResource.getContent(), false)));
-                }
-            }
+                        //recursion ahoy!
+                        bioSamplesClient.fetchSampleResource(relationship.getTarget())
+                                .ifPresent(sampleResource -> organisms.addAll(getOrganismsForSample(sampleResource.getContent(), false)));
+                    });
         }
 
         return organisms;
