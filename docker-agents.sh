c3b206ed783535b3a6def4695dd2ad42071c385b
Isuru Liyanage <theisuru@gmail.com>
2019-12-06 13:12:19 +0000

    Prepare for release of BioSamples v4.2.1

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-v4.2.1.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.1.jar
 echo "Successfully runned agents"
