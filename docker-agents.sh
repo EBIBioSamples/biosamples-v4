a5d869f07893feed60f39ecb027c0c89fa294f1b
dipayan1985 <dipayan.gupta0@gmail.com>
2020-01-15 22:17:10 +0000

    Fix issue in jar plugin version and move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.2-RC5.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.2-RC6.jar
 echo "Successfully runned agents"
