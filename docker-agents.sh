ce1a95eaf0d8fe235051d75d11f9755c41b1b8f5
dipayan1985 <dipayan.gupta0@gmail.com>
2020-01-15 16:54:03 +0000

    Fix issue in plexus and move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.2-RC3.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.2-RC4.jar
 echo "Successfully runned agents"
