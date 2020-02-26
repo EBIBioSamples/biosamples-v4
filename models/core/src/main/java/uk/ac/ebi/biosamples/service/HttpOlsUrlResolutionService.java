111d91f0b59c487a1a5d263e720826b0cc427d1a
dgupta <dgupta@ebi.ac.uk>
2020-02-26 09:54:26 +0000

    Use cache to avoid redundant calls

--- models/core/src/main/java/uk/ac/ebi/biosamples/service/HttpOlsUrlResolutionService.java
+++ models/core/src/main/java/uk/ac/ebi/biosamples/service/HttpOlsUrlResolutionService.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.biosamples.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponents;
@@ -39,6 +40,7 @@ public class HttpOlsUrlResolutionService {
      *
      * @return url representation of the IRI
      */
+    @Cacheable(value = "iri")
     public String getIriOls(final SortedSet<String> iri) {
         if (iri == null || iri.size() == 0) return null;
 
