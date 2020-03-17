18381049e814e70b4bea8ed9280cb4201745cc62
dgupta <dgupta@ebi.ac.uk>
2020-03-17 13:42:22 +0000

    Integrate Supp List generation in livelist pipeline

--- pipelines/livelist/src/main/java/uk/ac/ebi/biosamples/livelist/LiveListRunner.java
+++ pipelines/livelist/src/main/java/uk/ac/ebi/biosamples/livelist/LiveListRunner.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.biosamples.livelist;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.hateoas.Resource;
@@ -21,19 +22,23 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.GZIPOutputStream;
 
 @Component
 public class LiveListRunner implements ApplicationRunner {
     private static final Logger LOGGER = LoggerFactory.getLogger(LiveListRunner.class);
+    private final BioSamplesClient bioSamplesClient;
+    private final PipelinesProperties pipelinesProperties;
     public static final String INSDC_STATUS = "INSDC status";
     public static final String PUBLIC = "public";
     public static final String LIVE = "live";
-    private final BioSamplesClient bioSamplesClient;
-    private final PipelinesProperties pipelinesProperties;
 
-    public LiveListRunner(final BioSamplesClient bioSamplesClient, PipelinesProperties pipelinesProperties) {
+    @Autowired
+    private LiveListPipelineDao liveListPipelineDao;
+
+    public LiveListRunner(final BioSamplesClient bioSamplesClient, final PipelinesProperties pipelinesProperties) {
         this.pipelinesProperties = pipelinesProperties;
 
         if (bioSamplesClient.getPublicClient().isPresent()) {
@@ -45,20 +50,62 @@ public class LiveListRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        generateLiveListImport(args);
+        doLiveListExport(args);
+        doSuppListExport(args);
     }
 
-    private void generateLiveListImport(ApplicationArguments args) {
+    private void doSuppListExport(ApplicationArguments args) {
+        String suppListFileName = "supplist.txt";
+
+        if (args.getNonOptionArgs().size() > 0) {
+            suppListFileName = args.getNonOptionArgs().get(0);
+        }
+
+        long startTime = System.nanoTime();
+        Writer suppListWriter = null;
+
+        try {
+            suppListWriter = args.getOptionValues("gzip") == null
+                    ? new OutputStreamWriter(new FileOutputStream(suppListFileName), StandardCharsets.UTF_8)
+                    : new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(suppListFileName)), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            LOGGER.error("Failure to setup supp list writer", e);
+            MailSender.sendEmail("Live list pipeline - supplist generation", null, false);
+            System.exit(0);
+        }
+
+        doSuppListExport(startTime, suppListWriter);
+    }
+
+    private void doSuppListExport(final long startTime, final Writer suppListWriter) {
+        final AtomicInteger sampleCount = new AtomicInteger();
+        AtomicBoolean isPassed = new AtomicBoolean(true);
+
+        liveListPipelineDao.doGetSuppressedEnaSamples().forEach(sample -> {
+            try {
+                sampleCount.getAndIncrement();
+                suppListWriter.write(sample);
+                suppListWriter.write("\n");
+                suppListWriter.flush();
+            } catch (final Exception e) {
+                LOGGER.error("Pipeline failed to finish successfully", e);
+                isPassed.set(false);
+            } finally {
+                MailSender.sendEmail("Live list pipeline - livelist generation", null, isPassed.get());
+                long elapsed = System.nanoTime() - startTime;
+                LOGGER.info("Completed live list export:  " + sampleCount.get() + " samples exported in " + (elapsed / 1000000000L) + "s");
+            }
+        });
+    }
+
+    private void doLiveListExport(ApplicationArguments args) {
         String liveListFileName = "livelist.txt";
-        String liveListStatus = "";
 
         if (args.getNonOptionArgs().size() > 0) {
             liveListFileName = args.getNonOptionArgs().get(0);
         }
 
         long startTime = System.nanoTime();
-        AtomicInteger sampleCount = new AtomicInteger();
-
         Writer liveListWriter = null;
 
         try {
@@ -71,13 +118,15 @@ public class LiveListRunner implements ApplicationRunner {
             System.exit(0);
         }
 
-        doLiveListExport(liveListStatus, startTime, sampleCount, liveListWriter);
+        doLiveListExport(startTime, liveListWriter);
     }
 
-    private void doLiveListExport(String liveListStatus, long startTime, AtomicInteger sampleCount, Writer liveListWriter) {
+    private void doLiveListExport(final long startTime, final Writer liveListWriter) {
         LOGGER.info("Starting live list export");
         final Filter statusPublicFilter = FilterBuilder.create().onAttribute(INSDC_STATUS).withValue(PUBLIC).build();
         final Filter statusLiveFilter = FilterBuilder.create().onAttribute(INSDC_STATUS).withValue(LIVE).build();
+        final AtomicInteger sampleCount = new AtomicInteger();
+        String liveListStatus = "";
         boolean isPassed = true;
 
         try (liveListWriter; AdaptiveThreadPoolExecutor executorService = AdaptiveThreadPoolExecutor.create(100, 10000, true,
