946a833a0dd5cfb671fea180838abaa3a41f5b25
Isuru Liyanage <theisuru@gmail.com>
2020-04-28 07:16:46 +0100

    Move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.6-RC4.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.6-RC5.jar
 echo "Successfully runned agents"
