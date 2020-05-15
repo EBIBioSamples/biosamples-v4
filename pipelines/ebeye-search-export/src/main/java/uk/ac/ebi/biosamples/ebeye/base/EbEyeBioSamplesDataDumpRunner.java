f858cd718a00fbb943656f496d59268f3805be2e
dgupta <dgupta@ebi.ac.uk>
2020-05-15 09:43:16 +0100

    Fix gzip file command line argument

--- pipelines/ebeye-search-export/src/main/java/uk/ac/ebi/biosamples/ebeye/base/EbEyeBioSamplesDataDumpRunner.java
+++ pipelines/ebeye-search-export/src/main/java/uk/ac/ebi/biosamples/ebeye/base/EbEyeBioSamplesDataDumpRunner.java
@@ -49,8 +49,8 @@ public class EbEyeBioSamplesDataDumpRunner implements ApplicationRunner {
 
         // Fetch data by year - hardcoded for now
         // variable names are not correct now - FIX ME
-        final String from2017 ="2017.01.01";
-        final String until2018 ="2017.02.01";
+        final String from2017 = "2017.01.01";
+        final String until2018 = "2017.02.01";
         final File f2017 = new File("/mnt/data/biosamples/sw/wwwdev/biosd-ebeye-1.xml");
 
         fetchQueryAndDump(coll, from2017, until2018, f2017);
@@ -86,11 +86,11 @@ public class EbEyeBioSamplesDataDumpRunner implements ApplicationRunner {
 
     private static List<String> getAllDocuments(final DBCollection col, final String from, final String until) throws ParseException {
         final List<String> listOfAccessions = new ArrayList<>();
-        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat ("yyyy.MM.dd");
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
         final Date startDate = simpleDateFormat.parse(from);
         final Date endDate = simpleDateFormat.parse(until);
 
-        final BasicDBObject query = new BasicDBObject("release", new BasicDBObject("$gte",startDate).append("$lt",endDate ));
+        final BasicDBObject query = new BasicDBObject("release", new BasicDBObject("$gte", startDate).append("$lt", endDate));
         final DBCursor cursor = col.find(query).sort(new BasicDBObject("release", OrderBy.ASC.getIntRepresentation()));
 
         cursor.forEach(elem -> {
@@ -226,4 +226,155 @@ public class EbEyeBioSamplesDataDumpRunner implements ApplicationRunner {
     private String removeOtherSpecialCharactersFromAttributeNames(String type) {
         return type.trim().replaceAll("[^a-zA-Z0-9\\s+_-]", "");
     }
+
+    /*
+    // One time run for COVID-19 only
+    @Component
+public class EbEyeBioSamplesDataDumpRunner implements ApplicationRunner {
+    public static final String ENA_LC = "ena";
+    public static final String ENA_UC = "ENA";
+    @Autowired
+    BioSamplesClient bioSamplesClient;
+
+    public List<Sample> getSamplesList() {
+        Iterable<Resource<Sample>> sampleResources = bioSamplesClient.fetchSampleResourceAll("NCBITaxon_2697049");
+        List<Sample> sampleList = new ArrayList<>();
+
+        sampleResources.forEach(sampleResource -> sampleList.add(sampleResource.getContent()));
+
+        return sampleList;
+    }
+
+    public void convertSampleToXml(final List<Sample> samples, final File f) throws JAXBException {
+        DatabaseType databaseType = new DatabaseType();
+
+        databaseType.setName("BioSamples");
+        databaseType.setDescription("EBI BioSamples Database");
+        databaseType.setEntryCount(samples.size());
+        databaseType.setRelease("BioSamples COVID-19 Samples Release");
+        databaseType.setReleaseDate(new Date().toString());
+
+        AtomicReference<EntriesType> entriesType = new AtomicReference<>(new EntriesType());
+
+        samples.forEach(sample -> {
+            entriesType.set(getEntries(sample, entriesType.get()));
+            databaseType.setEntries(entriesType.get());
+        });
+
+        JAXBContext context = JAXBContext.newInstance(DatabaseType.class);
+
+        Marshaller jaxbMarshaller = context.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        jaxbMarshaller.marshal(databaseType, f);
+        jaxbMarshaller.marshal(databaseType, System.out);
+    }
+
+    private EntriesType getEntries(Sample sample, EntriesType entriesType) {
+        EntryType entryType = new EntryType();
+
+        getEntry(sample, entryType);
+        entriesType.getEntry().add(entryType);
+
+        return entriesType;
+    }
+
+    private void getEntry(Sample sample, EntryType entryType) {
+        entryType.setId(sample.getAccession());
+        entryType.setName(sample.getName());
+
+        AdditionalFieldsType additionalFieldsType = new AdditionalFieldsType();
+
+        getAdditionalFields(sample, entryType, additionalFieldsType);
+        entryType.setAdditionalFields(additionalFieldsType);
+
+        entryType.setDates(getDates(sample));
+        entryType.setCrossReferences(getCrossReferences(sample));
+    }
+
+    private CrossReferencesType getCrossReferences(Sample sample) {
+        CrossReferencesType crossReferencesType = new CrossReferencesType();
+
+        sample.getExternalReferences().forEach(extRef -> {
+            RefType refType = new RefType();
+
+            final var url = extRef.getUrl();
+
+            if (url.contains(ENA_LC) || url.contains(ENA_UC)) {
+                refType.setDbname(ENA_UC);
+                refType.setDbkey(extractEnaAccession(url));
+            }
+
+            crossReferencesType.getRef().add(refType);
+        });
+
+        crossReferencesType.getRef().add(getTaxonomyCrossReference(sample.getTaxId()));
+
+        return crossReferencesType;
+    }
+
+    private RefType getTaxonomyCrossReference(int taxId) {
+        RefType refType = new RefType();
+
+        refType.setDbname("TAXONOMY");
+        refType.setDbkey(String.valueOf(taxId));
+
+        return refType;
+    }
+
+    private String extractEnaAccession(String url) {
+        return url.substring(36);
+    }
+
+    private DatesType getDates(Sample sample) {
+        DatesType datesType = new DatesType();
+        DateType dateTypeRelease = new DateType();
+
+        dateTypeRelease.setType("release_date");
+        dateTypeRelease.setValue(sample.getReleaseDate());
+
+        DateType dateTypeUpdate = new DateType();
+
+        dateTypeUpdate.setType("update_date");
+        dateTypeUpdate.setValue(sample.getUpdateDate());
+
+        datesType.getDate().add(dateTypeRelease);
+        datesType.getDate().add(dateTypeUpdate);
+
+        return datesType;
+    }
+
+    private AdditionalFieldsType getAdditionalFields(Sample sample, EntryType entryType, AdditionalFieldsType additionalFieldsType) {
+        sample.getAttributes().forEach(attribute -> {
+            FieldType fieldType = new FieldType();
+
+            if (attribute.getType().equals("description")) {
+                entryType.setDescription(attribute.getValue());
+            } else {
+                fieldType.setName(removeOtherSpecialCharactersFromAttributeNames(removeSpacesFromAttributeNames(attribute.getType())));
+                fieldType.setValue(attribute.getValue());
+                additionalFieldsType.getFieldOrHierarchicalField().add(fieldType);
+            }
+        });
+
+        return additionalFieldsType;
+    }
+
+    private String removeSpacesFromAttributeNames(String type) {
+        return type.trim().replaceAll(" ", "_");
+    }
+
+    private String removeOtherSpecialCharactersFromAttributeNames(String type) {
+        return type.trim().replaceAll("[^a-zA-Z0-9\\s+_-]", "");
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        File f = new File("biosd-dump_cv_19.xml");
+        List<Sample> samplesList = getSamplesList();
+
+        convertSampleToXml(samplesList, f);
+    }
+}
+     */
 }
