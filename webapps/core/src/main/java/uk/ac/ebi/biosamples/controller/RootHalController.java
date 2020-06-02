1dccb5cb3e65b3d850830b1fc7fca88285ac52db
Isuru Liyanage <theisuru@gmail.com>
2020-06-02 11:12:49 +0100

    add sample group api to biosamples client, integration tests

--- webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/RootHalController.java
+++ webapps/core/src/main/java/uk/ac/ebi/biosamples/controller/RootHalController.java
@@ -1,8 +1,5 @@
 package uk.ac.ebi.biosamples.controller;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.ResourceSupport;
@@ -10,31 +7,30 @@ import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping
 public class RootHalController {
-	
-	public RootHalController() {
-	}
+
+    public RootHalController() {
+    }
 
     @CrossOrigin(methods = RequestMethod.GET)
-	@GetMapping(produces = { MediaTypes.HAL_JSON_VALUE })
-	public ResponseEntity<ResourceSupport> rootHal() {
-    	ResourceSupport resource = new ResourceSupport();    	
-    	    	
-    	resource.add(ControllerLinkBuilder.linkTo(SamplesRestController.class).withRel("samples"));
-    	resource.add(ControllerLinkBuilder.linkTo(CurationRestController.class).withRel("curations"));
-    	resource.add(new Link("https://www.ebi.ac.uk/data-protection/privacy-notice/embl-ebi-public-website", "privacyNotice"));
-    	resource.add(new Link("https://www.ebi.ac.uk/about/terms-of-use", "termsOfUse"));
-    	
-    	return ResponseEntity.ok()
-				.header(HttpHeaders.CACHE_CONTROL, CacheControl.maxAge(60, TimeUnit.MINUTES).cachePublic().getHeaderValue())
-				.body(resource);
+    @GetMapping(produces = {MediaTypes.HAL_JSON_VALUE})
+    public ResponseEntity<ResourceSupport> rootHal() {
+        ResourceSupport resource = new ResourceSupport();
+
+        resource.add(ControllerLinkBuilder.linkTo(SamplesRestController.class).withRel("samples"));
+        resource.add(ControllerLinkBuilder.linkTo(GroupsRestController.class).withRel("groups"));
+        resource.add(ControllerLinkBuilder.linkTo(CurationRestController.class).withRel("curations"));
+        resource.add(new Link("https://www.ebi.ac.uk/data-protection/privacy-notice/embl-ebi-public-website", "privacyNotice"));
+        resource.add(new Link("https://www.ebi.ac.uk/about/terms-of-use", "termsOfUse"));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CACHE_CONTROL, CacheControl.maxAge(60, TimeUnit.MINUTES).cachePublic().getHeaderValue())
+                .body(resource);
     }
 }
