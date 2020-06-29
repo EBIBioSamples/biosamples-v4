ef5e814b0fe65cf302054625261b8971054a9e85
Isuru Liyanage <theisuru@gmail.com>
2020-06-29 16:09:54 +0100

    First release candidate for BioSamples version 4.2.7

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.7-SNAPSHOT.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-4.2.7-RC1.jar
 echo "Successfully runned agents"
