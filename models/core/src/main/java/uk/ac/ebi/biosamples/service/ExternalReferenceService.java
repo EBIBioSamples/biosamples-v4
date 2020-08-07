6d4c91e708f6d231de3e5966ae7371d3d342fd0e
Isuru Liyanage <theisuru@gmail.com>
2020-08-07 17:36:35 +0100

    top level duo map

--- models/core/src/main/java/uk/ac/ebi/biosamples/service/ExternalReferenceService.java
+++ models/core/src/main/java/uk/ac/ebi/biosamples/service/ExternalReferenceService.java
@@ -8,6 +8,29 @@ import java.util.*;
 
 @Service
 public class ExternalReferenceService {
+    private static final Map<String, String> duoMap = new HashMap<>() {{
+        put("DUO:0000004", "No restriction");
+        put("DUO:0000005", "Research & clinical care");
+        put("DUO:0000006", "Health/(bio)medical/clinical care");
+        put("DUO:0000007", "Disease-specific & clinical care");
+        put("DUO:0000011", "Population origins & ancestry");
+        put("DUO:0000012", "Research-specific");
+        put("DUO:0000014", "Research use only");
+        put("DUO:0000015", "No general methods");
+        put("DUO:0000016", "Genetic studies only");
+        put("DUO:0000018", "Not-for-profit use only");
+        put("DUO:0000019", "Publication required");
+        put("DUO:0000020", "Collaboration required");
+        put("DUO:0000021", "Ethics approval required");
+        put("DUO:0000022", "Geographical restriction");
+        put("DUO:0000024", "Publication moratorium");
+        put("DUO:0000025", "Time limit on use");
+        put("DUO:0000026", "User-specific");
+        put("DUO:0000027", "Project-specific");
+        put("DUO:0000028", "Institution-specific ");
+        put("DUO:0000029", "Return to database/resource");
+    }};
+
     public String getNickname(ExternalReference externalReference) {
         return ExternalReferenceUtils.getNickname(externalReference);
     }
@@ -22,29 +45,6 @@ public class ExternalReferenceService {
 
 
     public String getDuoLabel(String duoCode) {
-        Hashtable<String, String> my_dict = new Hashtable<String, String>();
-
-        my_dict.put("DUO:0000004", "No restriction");
-        my_dict.put("DUO:0000005", "Research & clinical care");
-        my_dict.put("DUO:0000006", "Health/(bio)medical/clinical care");
-        my_dict.put("DUO:0000007", "Disease-specific & clinical care");
-        my_dict.put("DUO:0000011", "Population origins & ancestry");
-        my_dict.put("DUO:0000012", "Research-specific");
-        my_dict.put("DUO:0000014", "Research use only");
-        my_dict.put("DUO:0000015", "No general methods");
-        my_dict.put("DUO:0000016", "Genetic studies only");
-        my_dict.put("DUO:0000018", "Not-for-profit use only");
-        my_dict.put("DUO:0000019", "Publication required");
-        my_dict.put("DUO:0000020", "Collaboration required");
-        my_dict.put("DUO:0000021", "Ethics approval required");
-        my_dict.put("DUO:0000022", "Geographical restriction");
-        my_dict.put("DUO:0000024", "Publication moratorium");
-        my_dict.put("DUO:0000025", "Time limit on use");
-        my_dict.put("DUO:0000026", "User-specific");
-        my_dict.put("DUO:0000027", "Project-specific");
-        my_dict.put("DUO:0000028", "Institution-specific ");
-        my_dict.put("DUO:0000029", "Return to database/resource");
-
-    return my_dict.get(duoCode);
+        return duoMap.get(duoCode);
     }
 }
