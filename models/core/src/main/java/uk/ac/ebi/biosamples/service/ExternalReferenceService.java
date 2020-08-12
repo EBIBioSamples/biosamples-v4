cc9956e27bad6c1630d181a2bf473237dd5a272e
Isuru Liyanage <theisuru@gmail.com>
2020-08-12 11:44:49 +0100

    optimize code

--- models/core/src/main/java/uk/ac/ebi/biosamples/service/ExternalReferenceService.java
+++ models/core/src/main/java/uk/ac/ebi/biosamples/service/ExternalReferenceService.java
@@ -8,28 +8,7 @@ import java.util.*;
 
 @Service
 public class ExternalReferenceService {
-    private static final Map<String, String> duoMap = new HashMap<>() {{
-        put("DUO:0000004", "No restriction");
-        put("DUO:0000005", "Research & clinical care");
-        put("DUO:0000006", "Health/(bio)medical/clinical care");
-        put("DUO:0000007", "Disease-specific & clinical care");
-        put("DUO:0000011", "Population origins & ancestry");
-        put("DUO:0000012", "Research-specific");
-        put("DUO:0000014", "Research use only");
-        put("DUO:0000015", "No general methods");
-        put("DUO:0000016", "Genetic studies only");
-        put("DUO:0000018", "Not-for-profit use only");
-        put("DUO:0000019", "Publication required");
-        put("DUO:0000020", "Collaboration required");
-        put("DUO:0000021", "Ethics approval required");
-        put("DUO:0000022", "Geographical restriction");
-        put("DUO:0000024", "Publication moratorium");
-        put("DUO:0000025", "Time limit on use");
-        put("DUO:0000026", "User-specific");
-        put("DUO:0000027", "Project-specific");
-        put("DUO:0000028", "Institution-specific ");
-        put("DUO:0000029", "Return to database/resource");
-    }};
+    private static final Map<String, String> duoMap = Map.ofEntries(Map.entry("DUO:0000004", "No restriction"), Map.entry("DUO:0000005", "Research & clinical care"), Map.entry("DUO:0000006", "Health/(bio)medical/clinical care"), Map.entry("DUO:0000007", "Disease-specific & clinical care"), Map.entry("DUO:0000011", "Population origins & ancestry"), Map.entry("DUO:0000012", "Research-specific"), Map.entry("DUO:0000014", "Research use only"), Map.entry("DUO:0000015", "No general methods"), Map.entry("DUO:0000016", "Genetic studies only"), Map.entry("DUO:0000018", "Not-for-profit use only"), Map.entry("DUO:0000019", "Publication required"), Map.entry("DUO:0000020", "Collaboration required"), Map.entry("DUO:0000021", "Ethics approval required"), Map.entry("DUO:0000022", "Geographical restriction"), Map.entry("DUO:0000024", "Publication moratorium"), Map.entry("DUO:0000025", "Time limit on use"), Map.entry("DUO:0000026", "User-specific"), Map.entry("DUO:0000027", "Project-specific"), Map.entry("DUO:0000028", "Institution-specific "), Map.entry("DUO:0000029", "Return to database/resource"));
 
     public String getNickname(ExternalReference externalReference) {
         return ExternalReferenceUtils.getNickname(externalReference);
