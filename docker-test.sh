9a309173bab065a2eda0d38c2fe9b9eb7fdcd5cc
Dipayan Gupta <dgupta@ebi.ac.uk>
2020-12-11 18:15:24 +0530

    Third release candidate for BioSamples version v5.0.3

--- docker-test.sh
+++ docker-test.sh
@@ -5,7 +5,7 @@ docker-compose up -d biosamples-agents-solr
 
 for X in "$@"
 do
-  docker-compose run --rm --service-ports biosamples-integration java -jar integration-5.0.3-RC2.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-5.0.3-RC3.jar --phase=$X $ARGS $@
   sleep 30 #solr is configured to commit every 5 seconds
 
 done
