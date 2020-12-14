3dd9f34952af7dc4aeecfc1f671371c5118a1cd4
Dipayan Gupta <dgupta@ebi.ac.uk>
2020-12-14 19:51:25 +0530

    Fix issue in release note UI formatting and move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.0.3-RC3.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.0.3-RC4.jar
 echo "Successfully runned agents"
