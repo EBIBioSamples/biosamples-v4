0d902c767497e84aa8fe622653b563b349390e02
Isuru Liyanage <theisuru@gmail.com>
2021-05-09 23:56:18 +0530

    duo code rendering

--- models/core/src/main/java/uk/ac/ebi/biosamples/service/ExternalReferenceService.java
+++ models/core/src/main/java/uk/ac/ebi/biosamples/service/ExternalReferenceService.java
@@ -18,6 +18,13 @@ import uk.ac.ebi.biosamples.model.ExternalReference;
 
 @Service
 public class ExternalReferenceService {
+  private final Map<String, Map<String, String>> ontologyMap;
+
+  public ExternalReferenceService() {
+    ontologyMap = new HashMap<>();
+    populateOntologies(ontologyMap);
+  }
+
   public String getNickname(ExternalReference externalReference) {
     return ExternalReferenceUtils.getNickname(externalReference);
   }
@@ -30,11 +37,21 @@ public class ExternalReferenceService {
     return ExternalReferenceUtils.getDuoUrl(duoCode);
   }
 
-  public Map<String, String> getOntologyDescription(String ontologyId) {
-    Map<String, String> ontology = new HashMap<>();
-    ontology.put("header", "");
-    ontology.put("body", "body");
+  public String getOntologyTitle(String ontologyId) {
+    return ontologyMap.get(ontologyId).get("title");
+  }
 
-    return ontology;
+  public String getOntologyDescription(String ontologyId) {
+    return ontologyMap.get(ontologyId).get("body");
+  }
+
+  private void populateOntologies(Map<String, Map<String, String>> ontologyMap) {
+    ontologyMap.put("DUO:0000001", Map.of("title", "Data use permission", "body", "A data item that is used to indicate consent permissions for datasets and/or materials, and relates to the purposes for which datasets and/or material might be removed, stored or used"));
+    ontologyMap.put("DUO:0000005", Map.of("title", "Obsolete general research use and clinical care", "body", "This data use limitation indicates that use is allowed for health/medical/biomedical purposes and other biological research, including the study of population origins or ancestry"));
+    ontologyMap.put("DUO:0000007", Map.of("title", "Disease specific research", "body", "This term should be coupled with a term describing a disease from an ontology to specify the disease the restriction applies to"));
+    ontologyMap.put("DUO:0000014", Map.of("title", "Obsolete research use only", "body", "This data use limitation indicates that use is limited to research purposes"));
+    ontologyMap.put("DUO:0000019", Map.of("title", "Publication required", "body", "This data use modifier indicates that requestor agrees to make results of studies using the data available to the larger scientific community"));
+    ontologyMap.put("DUO:0000026", Map.of("title", "User specific restriction", "body", "This data use modifier indicates that use is limited to use by approved users."));
+    ontologyMap.put("DUO:0000028", Map.of("title", "Institution specific restriction", "body", "This data use modifier indicates that use is limited to use within an approved institution"));
   }
 }
