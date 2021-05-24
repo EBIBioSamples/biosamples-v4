fac1e50893a8470eb6e29fbb97c61defa0ec676a
Dipayan Gupta <dgupta@ebi.ac.uk>
2021-05-24 18:27:13 +0100

    Non issue but increment version for Bamboo and move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.1.1-RC2.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.1.1-RC3.jar
 echo "Successfully runned agents"
