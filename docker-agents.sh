45f8d7b8f9b65390076691923dc52d1d507f3334
Dipayan Gupta <dgupta@ebi.ac.uk>
2021-05-25 16:20:09 +0100

    Prepare for release of BioSamples 5.1.1

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.1.1-RC6.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.1.1.jar
 echo "Successfully runned agents"
