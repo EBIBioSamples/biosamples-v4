a07a91e4e3c1dc753cdd6fb374d6b054ad07b141
Dipayan Gupta <dgupta@ebi.ac.uk>
2021-06-30 11:59:14 +0100

    Increment version just for gromit and move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.1.4-RC7.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.1.4-RC8.jar
 echo "Successfully runned agents"
