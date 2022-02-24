61f39aa5f842527d564049f1ea3f1013d20bdc63
Isuru Liyanage <theisuru@gmail.com>
2022-02-24 11:04:04 +0000

    Fix legacy strucuted data deserialization error and move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.2.1-RC1.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.2.1-RC2.jar
 echo "Successfully runned agents"
