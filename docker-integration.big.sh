80876750bf7396f3d15283a525b72dd49da2f36f
Dipayan Gupta <dgupta@ebi.ac.uk>
2022-05-05 10:55:10 +0100

    Move to next version of the software 5.2.4-SNAPSHOT

--- docker-integration.big.sh
+++ docker-integration.big.sh
@@ -11,7 +11,7 @@ ARGS=--spring.profiles.active=big
 for X in 1 2 3 4 5
 do
   #java -jar integration/target/integration-4.0.0-SNAPSHOT.jar --phase=$X $ARGS $@
-  docker-compose run --rm --service-ports biosamples-integration java -jar integration-5.2.3-SNAPSHOT.jar --phase=$X $ARGS $@
+  docker-compose run --rm --service-ports biosamples-integration java -jar integration-5.2.4-SNAPSHOT.jar --phase=$X $ARGS $@
   sleep 10 #solr is configured to commit every 5 seconds
 
 done
