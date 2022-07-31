27528eb9e8aff83bd2ab3c939b6c88e38391bce1
dgupta <dgupta@ebi.ac.uk>
2022-07-31 14:43:02 +0100

    Re-merge dev to master and move to next release candidate

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.2.8-SNAPSHOT.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.2.7-RC3.jar
 echo "Successfully runned agents"
