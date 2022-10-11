678485a85b98515cb77b9d17674ebe2782dfe83c
dgupta <dgupta@ebi.ac.uk>
2022-10-11 15:54:37 +0100

    First release candidate for BioSamples version 5.2.11

--- docker-agents.sh
+++ docker-agents.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.2.11-SNAPSHOT.jar
+docker-compose run --rm --service-ports biosamples-agents-solr java -jar agents-solr-5.2.11-RC1.jar
 echo "Successfully runned agents"
