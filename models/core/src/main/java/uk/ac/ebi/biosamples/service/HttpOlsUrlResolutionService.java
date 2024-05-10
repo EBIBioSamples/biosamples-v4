b293fc00fdee60cc7124f0b875b82889ddc7699f
Isuru Liyanage <theisuru@gmail.com>
2024-05-10 06:15:55 +0100

    use new OLS API to point to ontology links

--- models/core/src/main/java/uk/ac/ebi/biosamples/service/HttpOlsUrlResolutionService.java
+++ models/core/src/main/java/uk/ac/ebi/biosamples/service/HttpOlsUrlResolutionService.java
@@ -1,32 +1,49 @@
 /*
-* Copyright 2021 EMBL - European Bioinformatics Institute
-* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
-* file except in compliance with the License. You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software distributed under the
-* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-* CONDITIONS OF ANY KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations under the License.
-*/
+ * Copyright 2021 EMBL - European Bioinformatics Institute
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package uk.ac.ebi.biosamples.service;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.SortedSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.SortedSet;
+
 @Service
 public class HttpOlsUrlResolutionService {
-  private static final String OLS_PREFIX = "http://www.ebi.ac.uk/ols/terms?iri=";
-  private static final String OBO = "purl.obolibrary.org/obo";
-  private static final String EBIUK = "www.ebi.ac.uk";
+  private static final String OLS_PREFIX = "https://www.ebi.ac.uk/ols4?termId=";
+  private static final String OBO_URL_SEGMENT = "purl.obolibrary.org/obo";
+  private static final String EBI_URL_SEGMENT = "www.ebi.ac.uk";
   public static Logger log = LoggerFactory.getLogger(HttpOlsUrlResolutionService.class);
 
-  public HttpOlsUrlResolutionService() {}
+  public HttpOlsUrlResolutionService() {
+  }
+
+  public static boolean isCurie(String term) {
+    String[] segments;
+    if (term.contains(":")) {
+      segments = term.split(":");
+    } else if (term.contains("_")) {
+      segments = term.split("_");
+    } else {
+      return false;
+    }
+
+    return segments.length == 2 && segments[0].matches("[a-zA-Z]+") && segments[1].matches("\\d+");
+  }
+
+  private static String formatCurie(String displayIri) {
+    return displayIri.trim().replace("_", ":");
+  }
 
   /**
    * This returns a string representation of the URL to lookup the associated ontology term iri in
@@ -42,6 +59,10 @@ public class HttpOlsUrlResolutionService {
 
     final String displayIri = iri.first();
 
+    if (isCurie(displayIri)) {
+      return formatCurie(displayIri);
+    }
+
     // check this is a sane iri
     try {
       final UriComponents iriComponents =
@@ -54,12 +75,11 @@ public class HttpOlsUrlResolutionService {
         return null;
       }
 
-      // TODO application.properties this
-      // TODO use https
-      final String iriUrl = URLEncoder.encode(displayIri, StandardCharsets.UTF_8.toString());
+      String[] segments = displayIri.split("/");
+      String curie = segments[segments.length - 1].replace("_", ":");
 
       if (checkUrlForPattern(displayIri)) {
-        return OLS_PREFIX + iriUrl;
+        return OLS_PREFIX + curie;
       } else {
         return displayIri;
       }
@@ -71,10 +91,6 @@ public class HttpOlsUrlResolutionService {
   }
 
   private boolean checkUrlForPattern(final String displayIri) {
-    if (displayIri.contains(OBO) || displayIri.contains(EBIUK)) {
-      return true;
-    } else {
-      return false;
-    }
+    return displayIri.contains(OBO_URL_SEGMENT) || displayIri.contains(EBI_URL_SEGMENT);
   }
 }
