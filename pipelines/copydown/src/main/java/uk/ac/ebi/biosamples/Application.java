17ee4f15d39741536bb157bc25abc6633487be0c
Isuru Liyanage <theisuru@gmail.com>
2025-12-12 07:47:33 +0000

    fix: repo config for pipelines

--- pipelines/copydown/src/main/java/uk/ac/ebi/biosamples/Application.java
+++ pipelines/copydown/src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -67,86 +67,17 @@ public class Application {
   }
 
   @Bean
+  public RestTemplate restTemplate(final RestTemplateCustomizer restTemplateCustomizer) {
+    final RestTemplate restTemplate = new RestTemplate();
+    restTemplateCustomizer.customize(restTemplate);
+    return restTemplate;
+  }
+
+  @Bean
   public RestTemplateCustomizer restTemplateCustomizer(
       final BioSamplesProperties bioSamplesProperties,
-      final PipelinesProperties piplinesProperties) {
-    return new RestTemplateCustomizer() {
-      @Override
-      public void customize(final RestTemplate restTemplate) {
-
-        // use a keep alive strategy to try to make it easier to maintain connections for
-        // reuse
-        final ConnectionKeepAliveStrategy keepAliveStrategy =
-            new ConnectionKeepAliveStrategy() {
-              @Override
-              public long getKeepAliveDuration(
-                  final HttpResponse response, final HttpContext context) {
-
-                // check if there is a non-standard keep alive header present
-                final HeaderElementIterator it =
-                    new BasicHeaderElementIterator(response.headerIterator(HTTP.CONN_KEEP_ALIVE));
-                while (it.hasNext()) {
-                  final HeaderElement he = it.nextElement();
-                  final String param = he.getName();
-                  final String value = he.getValue();
-                  if (value != null && param.equalsIgnoreCase("timeout")) {
-                    return Long.parseLong(value) * 1000;
-                  }
-                }
-                // default to 60s if no header
-                return 60 * 1000;
-              }
-            };
-
-        // set a number of connections to use at once for multiple threads
-        final PoolingHttpClientConnectionManager poolingHttpClientConnectionManager =
-            new PoolingHttpClientConnectionManager();
-        poolingHttpClientConnectionManager.setMaxTotal(piplinesProperties.getConnectionCountMax());
-        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(
-            piplinesProperties.getConnectionCountDefault());
-        poolingHttpClientConnectionManager.setMaxPerRoute(
-            new HttpRoute(HttpHost.create(piplinesProperties.getZooma())),
-            piplinesProperties.getConnectionCountZooma());
-        poolingHttpClientConnectionManager.setMaxPerRoute(
-            new HttpRoute(HttpHost.create(bioSamplesProperties.getOls())),
-            piplinesProperties.getConnectionCountOls());
-
-        // set a local cache for cacheable responses
-        final CacheConfig cacheConfig =
-            CacheConfig.custom()
-                .setMaxCacheEntries(1024)
-                .setMaxObjectSize(1024 * 1024) // max size of 1Mb
-                // number of entries x size of entries = 1Gb total cache size
-                .setSharedCache(false) // act like a browser cache not a middle-hop cache
-                .build();
-
-        // set a timeout limit
-        // TODO put this in application.properties
-        final int timeout = 60; // in seconds
-        final RequestConfig config =
-            RequestConfig.custom()
-                .setConnectTimeout(timeout * 1000) // time to establish the connection with the
-                // remote host
-                .setConnectionRequestTimeout(
-                    timeout * 1000) // maximum time of inactivity between two
-                // data packets
-                .setSocketTimeout(timeout * 1000)
-                .build(); // time to wait for a connection from the connection
-        // manager/pool
-
-        // make the actual client
-        final HttpClient httpClient =
-            CachingHttpClientBuilder.create()
-                .setCacheConfig(cacheConfig)
-                .useSystemProperties()
-                .setConnectionManager(poolingHttpClientConnectionManager)
-                .setKeepAliveStrategy(keepAliveStrategy)
-                .setDefaultRequestConfig(config)
-                .build();
-
-        // and wire it into the resttemplate
-        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
-      }
-    };
+      final PipelinesProperties pipelinesProperties) {
+    return new PipelinesHelper()
+        .getRestTemplateCustomizer(bioSamplesProperties, pipelinesProperties);
   }
 }
